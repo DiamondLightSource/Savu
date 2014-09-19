d87e26ad4e68806708bbdb81d15f912bc08cf582
Mark Basham <mark.basham@diamond.ac.uk>
2014-09-19 10:25:03 +0100

    Added a new launcher inside eclipse for running the mpi test

--- mpi/dls/launcher.sh
+++ mpi/dls/launcher.sh
@@ -1,3 +1,8 @@
 module load global/cluster
 
-qsub -j y -pe openmpi 8 -q medium.q@@com01 /home/ssg37927/Savu/mpi/dls/mpijob.sh $@
+qsub -sync y -j y -pe openmpi 8 -q medium.q@@com01 /home/ssg37927/Savu/mpi/dls/mpijob.sh $@ > tmp.txt
+
+filename=`echo mpirun.sh.o`
+filename=$filename`awk '{print $3}' tmp.txt`
+
+cat $filename
