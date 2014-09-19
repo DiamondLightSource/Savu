e4d39b36ef0f24b15998214661929713856a8241
Mark Basham <mark.basham@diamond.ac.uk>
2014-09-19 10:58:06 +0100

    updates to mpi launcher to make it more interactive

--- mpi/dls/launcher.sh
+++ mpi/dls/launcher.sh
@@ -1,8 +1,13 @@
 module load global/cluster
 
-qsub -sync y -j y -pe openmpi 8 -q medium.q@@com01 /home/ssg37927/Savu/mpi/dls/mpijob.sh $@ > tmp.txt
+qsub -sync y -j y -pe openmpi 16 -q medium.q@@com01 /home/ssg37927/Savu/mpi/dls/mpijob.sh $@ > tmp.txt
 
-filename=`echo mpirun.sh.o`
+filename=`echo mpijob.sh.o`
 filename=$filename`awk '{print $3}' tmp.txt`
+
+while [ ! -f $filename ]
+do
+  sleep 2
+done
 
 cat $filename
