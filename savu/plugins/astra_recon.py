Update to fix the center of rotation issues introduced previously

savu/plugins/astra_recon.py
@@ -55,8 +55,7 @@ class AstraRecon(BaseRecon, CpuPlugin):
             plow = (blen - alen) + pad
             phigh = pad
 
-        sino = (sino-sino.min())+1
-        logdata = -np.log(sino)
+        logdata = np.log(sino+1)
         sinogram = np.pad(logdata, ((0, 0), (plow, phigh)), mode='reflect')
 
         width = sinogram.shape[1]

savu/plugins/base_recon.py
@@ -53,8 +53,8 @@ class BaseRecon(Plugin):
         """
         centre_of_rotation = data.center_of_rotation[:]
         if centre_of_rotation is None:
-            centre_of_rotation = np.zeros(data.get_number_of_sinograms())
-            centre_of_rotation *= self.parameters['center_of_rotation']
+            centre_of_rotation = np.ones(data.get_number_of_sinograms())
+            centre_of_rotation = centre_of_rotation * self.parameters['center_of_rotation']
 
         sinogram_frames = np.arange(data.get_number_of_sinograms())
 
@@ -67,7 +67,6 @@ class BaseRecon(Plugin):
         for i in range(len(frames)):
             frame_centre_of_rotation = centre_of_rotations[i]
             sinogram = data.data[:, frames[i], :]
-            sinogram = np.log(sinogram)
             reconstruction = \
                 self.reconstruct(sinogram, frame_centre_of_rotation, angles,
                                  (output.data.shape[0], output.data.shape[2]),

savu/plugins/filter.py
@@ -51,17 +51,19 @@ class Filter(Plugin):
 
         width = self.get_filter_width()
         for frame in frames:
-            min = frame-width
-            max = frame+width+1
+            minval = frame-width
+            maxval = frame+width+1
             minpad = 0
             maxpad = 0
-            if min < 0:
-                minpad = min*-1
-                min = 0
-            if max > data.data.shape[0]:
-                maxpad = (max-data.data.shape[0]) + 1
-                max = data.data.shape[0] - 1
-            projection = data.data[min:max, :, :]
+            if minval < 0:
+                minpad = minval*-1
+                minval = 0
+            if maxval > data.data.shape[0]:
+                maxpad = (maxval-data.data.shape[0]) + 1
+                maxval = data.data.shape[0] - 1
+            projection = data.data[minval:maxval, :, :]
+            logging.debug("projection shape is %s", str(projection.shape))
+            logging.debug("max and min are %i, %i", minval, maxval)
             projection = np.pad(projection, ((minpad, maxpad), (0, 0), (0, 0)),
                                 mode='edge')
             result = self.filter_frame(projection)

savu/plugins/simple_recon.py
@@ -55,12 +55,14 @@ class SimpleRecon(BaseRecon, CpuPlugin):
 
     def reconstruct(self, sinogram, centre_of_rotation, angles, shape, center):
         result = np.zeros(shape)
+        sino = np.nan_to_num(sinogram)
+        sino = np.log(sino+1)
         for i in range(sinogram.shape[0]):
             theta = i * (np.pi/sinogram.shape[0])
             mapping_array = self._mapping_array(shape, center, theta)
             filt = np.zeros(sinogram.shape[1]*3)
             filt[sinogram.shape[1]:sinogram.shape[1]*2] = \
-                self._filter(sinogram[i, :])
+                self._filter(sino[i, :])
             result += \
                 self._back_project(mapping_array, filt,
                                    (centre_of_rotation + sinogram.shape[1]))