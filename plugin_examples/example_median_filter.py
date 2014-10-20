7876f0054904c2ae696de4ca35cef53fac6cfa00
Mark Basham <mark.basham@diamond.ac.uk>
2014-10-20 21:59:52 +0100

    Added an example median filter example for a user defined filter

---
+++ plugin_examples/example_median_filter.py
@@ -0,0 +1,53 @@
+# Copyright 2014 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. module:: median_3x3_filter
+   :platform: Unix
+   :synopsis: A plugin to filter each frame with a 3x3 median filter
+
+.. moduleauthor:: Mark Basham <scientificsoftware@diamond.ac.uk>
+
+"""
+import logging
+
+from savu.plugins.filter import Filter
+from savu.plugins.cpu_plugin import CpuPlugin
+
+import scipy.signal.signaltools as sig
+
+
+class ExampleMedianFilter(Filter, CpuPlugin):
+    """
+    A plugin to filter each frame with a 3x3 median filter
+    """
+
+    def __init__(self):
+        logging.debug("initialising Example Median Filter")
+        super(ExampleMedianFilter,
+              self).__init__("ExampleMedianFilter")
+
+    def populate_default_parameters(self):
+        logging.debug("Populating Example Median Filter default parameters")
+        self.parameters['kernel_size'] = (3, 3, 3)
+
+    def get_filter_width(self):
+        logging.debug("Getting the filter width of Example Median Filter")
+        width = (self.parameters['kernel_size'][0]-1)/2
+        return width
+
+    def filter_frame(self, data):
+        logging.debug("Getting the filter frame of Example Median Filter")
+        result = sig.medfilt(data, self.parameters['kernel_size'])
+        return result[result.shape[0]/2, :, :]
