8205d8deeaabdff450b929352c10ecce54aa2cb0
Nghia Vo <wtc11931@ws123.diamond.ac.uk>
2015-04-27 16:48:07 +0100

    Padding the data

--- savu/plugins/paganin_filter.py
+++ savu/plugins/paganin_filter.py
@@ -9,6 +9,18 @@ import math
 
 
 class PaganinFilter(Filter, CpuPlugin):
+    """
+    A plugin to apply Paganin filter (contrast enhancement) on projections
+    
+    :param Energy: Given X-ray energy in keV. Default: 20.
+    :param Distance: Distance from sample to detection - Unit is metre. Default: 1.0.
+    :param Resolution: Pixel size - Unit is micron. Default: 21.46.
+    :param Ratio: ratio of delta/beta. Default: 10.0.
+    :param Padtopbottom: Pad to the top and bottom of projection. Default: 10.
+    :param Padleftright: Pad to the left and right of projection. Default: 10.
+    :param Padmethod: Method of padding. Default: 'edge'.
+    """
+    
     def __init__(self):
         logging.debug("initialising Paganin Filter")
         logging.debug("Calling super to make sure that all superclases are " +
@@ -17,32 +29,8 @@ class PaganinFilter(Filter, CpuPlugin):
               self).__init__("PaganinFilter")
         self.filtercomplex = None
 
-    def populate_default_parameters(self):
-        """
-        All plugins have the populate_default_patamters method, in this you
-        need to add to the self.parameters dictionary any parameters which you
-        wish the end user to ultimately be able to change, in this case we will
-        let them define the size of the kernel we will use for out 3D median
-        filter. We initialise this with a good default value, in this case a
-        tuple of (3, 3, 3) If you wish to change the direction that the filter
-        travels across the data, then you need to adjust the
-        self.parameters['slice_direction'] = 0
-        entry to be a different value,
-        i.e. 1 for sinogram direction.
-        """
-        logging.debug("Populating parameters from superclasses, must be done!")
-        super(PaganinFilter, self).populate_default_parameters()
-        logging.debug("Populating Paganin Filter default parameters")
-        # List of parameter  (Energy,distance,resolution,ratio,padding)
-        self.parameters['Energy'] = 20.0  # keV
-        self.parameters['Distance'] = 1.0  # m
-        self.parameters['Resolution'] = 21.46  # micron
-        self.parameters['Ratio'] = 10.0  # ratio of delta/beta
-
     def _setup_paganin(self, width, height):
         if self.filtercomplex is None:
-            centery = np.ceil(height/2.0)-1.0
-            centerx = np.ceil(width/2.0)-1.0
             micron = 10**(-6)
             keV = 1000.0
             distance = self.parameters['Distance']
@@ -50,22 +38,26 @@ class PaganinFilter(Filter, CpuPlugin):
             resolution = self.parameters['Resolution']*micron
             wavelength = (1240.0/energy)*10.0**(-9)
             ratio = self.parameters['Ratio']
+            padtopbottom = self.parameters['Padtopbottom']
+            padleftright = self.parameters['Padleftright']
+            height1 = height + 2*padtopbottom
+            width1 = width + 2*padleftright
+            centery = np.ceil(height1/2.0)-1.0
+            centerx = np.ceil(width1/2.0)-1.0
             # Define the paganin filter
-            dpx = 1.0/(width*resolution)
-            dpy = 1.0/(height*resolution)
-            pxlist = (np.arange(width)-centerx)*dpx
-            pylist = (np.arange(height)-centery)*dpy
-            pxx = np.zeros((height, width), dtype=np.float32)
-            pxx[:, 0:width] = pxlist
-            pyy = np.zeros((height, width), dtype=np.float32)
-            pyy[0:height, :] = np.reshape(pylist, (height, 1))
+            dpx = 1.0/(width1*resolution)
+            dpy = 1.0/(height1*resolution)
+            pxlist = (np.arange(width1)-centerx)*dpx
+            pylist = (np.arange(height1)-centery)*dpy
+            pxx = np.zeros((height1, width1), dtype=np.float32)
+            pxx[:, 0:width1] = pxlist
+            pyy = np.zeros((height1, width1), dtype=np.float32)
+            pyy[0:height1, :] = np.reshape(pylist, (height1, 1))
             pd = (pxx*pxx+pyy*pyy)*wavelength*distance*math.pi
             filter1 = 1.0+ratio*pd
             self.filtercomplex = filter1+filter1*1j
 
     def _paganin(self, data, axes):
-        data = np.nan_to_num(data)  # Noted performance
-        data[data == 0] = 1.0
         pci1 = np.fft.fft2(np.float32(data))
         pci2 = np.fft.fftshift(pci1)/self.filtercomplex
         fpci = np.abs(np.fft.ifft2(pci2))
@@ -76,8 +68,15 @@ class PaganinFilter(Filter, CpuPlugin):
         logging.debug("Getting the filter frame of Paganin Filter")
         (depth, height, width) = data.shape
         self._setup_paganin(width, height)
+        data = np.nan_to_num(data)  # Noted performance
+        data[data == 0] = 1.0
+        padtopbottom = self.parameters['Padtopbottom']
+        padleftright = self.parameters['Padleftright']
+        padmethod = self.parameters['Padmethod']
+        #data1 = np.lib.pad(data, ((padtopbottom, padtopbottom), (padleftright, padleftright)), padmethod)
+        data = np.lib.pad(data, ((0, 0), (padtopbottom, padtopbottom), (padleftright, padleftright)), padmethod)
         result = np.apply_over_axes(self._paganin, data, 0)
-        return result[result.shape[0]/2, :, :]
+        return result[result.shape[0]/2, padtopbottom:-padtopbottom, padleftright: -padleftright]
 
     def required_data_type(self):
         """
