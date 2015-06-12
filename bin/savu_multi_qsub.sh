2399424e4a7a9c4138b82e74c7149fec41fc1e38
nicwade <nicolawadeson@gmail.com>
2015-06-12 14:24:43 +0100

    amending error in CPU list

--- bin/savu_multi_qsub.sh
+++ bin/savu_multi_qsub.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
-echo "Enter the number of nodes to use:"
-read nNodes
+#echo "Enter the number of nodes to use:"
+#read nNodes
 
-echo "Enter the number of CPUs per node:"
-read nCPUs
+#echo "Enter the number of CPUs per node:"
+#read nCPUs
 
-echo "Enter the number of runs for averaging:"
-read nRuns
+#echo "Enter the number of runs for averaging:"
+#read nRuns
 
-echo "Enter start of output filename:"
-read fname
+#echo "Enter start of output filename:"
+#read fname
 
-clear 
+#clear 
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-x=$DIR
-savupath=${x%/bin}
+while read -r a b c d; do
+	nNodes=$a
+	nCPUs=$b
+	nRuns=$c
+	fname=$d
 
-runfile=/bin/savu_launcher.sh
-datafile=/test_data/24737.nxs
-processfile=/test_data/process01.nxs
-outpath=$PWD #outputting to the current folder
-outname="${fname}N${nNodes}_C${nCPUs}_mpi_test"
+	DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	x=$DIR
+	savupath=${x%/bin}
 
-for i in $(eval echo {1..$nRuns})
-  do
-    $savupath$runfile $savupath $datafile $processfile $outpath $outname $nNodes $nCPUs
-done
+	runfile=/bin/savu_launcher.sh
+	datafile=/test_data/24737.nxs
+	processfile=/test_data/process01.nxs
+	outpath=$PWD #outputting to the current folder
+	outname="${fname}N${nNodes}_C${nCPUs}_mpi_test"
 
-python $savupath/scripts/log_evaluation/GraphicalThreadProfiler_multi.py "$outpath/"log_${outname}*""
+	for i in $(eval echo {1..$nRuns})
+  		do
+   		 $savupath$runfile $savupath $datafile $processfile $outpath $outname $nNodes $nCPUs
+	done
+
+	python $savupath/scripts/log_evaluation/GraphicalThreadProfiler_multi.py "$outpath/"log_${outname}*""
+
+done < ../input_params.txt
 
