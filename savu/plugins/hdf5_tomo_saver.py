759b320b3c6f6d1f11289eba59bf8c13a642987a
Mark Basham <mark.basham@diamond.ac.uk>
2015-08-18 22:16:41 +0100

    initial work on getting a test runner in place for the new pipeline

--- savu/plugins/hdf5_tomo_saver.py
+++ savu/plugins/hdf5_tomo_saver.py
@@ -26,6 +26,10 @@ from savu.core.utils import logmethod
 from savu.plugins.base_saver import BaseSaver
 import savu.data.transport_data.standard_savers as sSaver
 
+from savu.plugins.utils import register_plugin
+
+
+@register_plugin
 class Hdf5TomoSaver(BaseSaver):
     """
     A class to save tomography data to a hdf5 file
