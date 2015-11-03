fa0de21b681b535dfe844a76a4306febbff68243
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2015-11-03 17:28:27 +0000

    changes

--- bin/savu_mpijob.sh
+++ bin/savu_mpijob.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#module load global/testcluster
-module load global/cluster
+module load global/testcluster
+#module load global/cluster
 module load python/ana
 #source activate mpi2
-source activate mpi4
+source activate mpi5
 module load openmpi/1.6.5
+export PYTHONPATH=$PYTHONPATH:/home/clb02321/DAWN_stable/FastXRF/src/
 #export LD_LIBRARY_PATH=/dls_sw/prod/tools/RHEL6-x86_64/openmpi/1-6-5/prefix/lib:$LD_LIBRARY_PATH
 
 savupath=$1
