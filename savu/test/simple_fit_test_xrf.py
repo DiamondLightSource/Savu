b9179c760c95977e505b6ac3841c6c9c13fb715d
nicwade <nicolawadeson@gmail.com>
2015-11-13 15:57:38 +0000

    fixing more tests

--- savu/test/simple_fit_test_xrf.py
+++ savu/test/simple_fit_test_xrf.py
@@ -35,7 +35,8 @@ class SimpleFitTestXrf(unittest.TestCase):
             "transport": "hdf5",
             "process_names": "CPU0",
             "data_file": tu.get_test_data_path('mm.nxs'),
-            "process_file": tu.get_test_data_path('simplefittestXRF.nxs'),
+            "process_file": tu.get_test_process_path(
+                'simple_fit_test_XRF.nxs'),
             "out_path": tempfile.mkdtemp()
             }
         run_protected_plugin_runner(options)
