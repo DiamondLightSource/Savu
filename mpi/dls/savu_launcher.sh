836ea32d63e512ca942ce390bcfdf20e62bb363f
Mark Basham <mark.basham@diamond.ac.uk>
2015-12-01 12:05:56 +0000

    dont cd to a local dir

--- mpi/dls/savu_launcher.sh
+++ mpi/dls/savu_launcher.sh
@@ -3,10 +3,6 @@ module load global/cluster
 
 echo "SAVU_LAUNCHER:: Running Job"
 
-cd /dls/tmp/savu/
-
-echo "SAVU_LAUNCHER:: Changed to temporary directory - /dls/tmp/savu"
-
 savupath=/dls_sw/apps/savu/master/Savu
 datafile=$1
 processfile=$2
@@ -18,14 +14,12 @@ nCPUs=20
 filepath=$savupath/bin/savu_mpijob.sh
 M=$((nNodes*20))
 
-echo "SAVU_LAUNCHER:: Running Job, please wait"
-
-qsub -N $outname -sync y -j y -pe openmpi $M -l infiniband -q medium.q@@com10 $filepath $savupath $datafile $processfile $outpath $nCPUs > $USER.out
+qsub -N $outname -sync y -j y -o /dls/tmp/savu/$USER -e /dls/tmp/savu/$USER -pe openmpi $M -l infiniband -q medium.q@@com10 $filepath $savupath $datafile $processfile $outpath $nCPUs > /dls/tmp/savu/$USER.out
 
 echo "SAVU_LAUNCHER:: Job Complete, preparing output..."
 
 filename=`echo $outname.o`
-jobnumber=`awk '{print $3}' $USER.out | head -n 1`
+jobnumber=`awk '{print $3}' /dls/tmp/savu/$USER.out | head -n 1`
 filename=$filename$jobnumber
 
 while [ ! -f $filename ]
