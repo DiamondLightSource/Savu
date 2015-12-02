1d329ca1f7287a0acac9b2071f1ef4d4f9d87511
Mark Basham <mark.basham@diamond.ac.uk>
2015-12-02 16:07:54 +0000

    full implementation tab completion

--- scripts/config_generator/savu_config.py
+++ scripts/config_generator/savu_config.py
@@ -11,6 +11,9 @@ from savu.plugins import utils as pu
 import pkgutil
 import savu
 import readline
+import re
+
+RE_SPACE = re.compile('.*\s+$', re.M)
 
 
 class Content(object):
@@ -182,8 +185,80 @@ commands = {'open': _open,
             'add': _add,
             'rem': _rem}
 
+
+class Completer(object):
+
+    def _listdir(self, root):
+        "List directory 'root' appending the path separator to subdirs."
+        res = []
+        for name in os.listdir(root):
+            path = os.path.join(root, name)
+            if os.path.isdir(path):
+                name += os.sep
+            res.append(name)
+        return res
+
+    def _complete_path(self, path=None):
+        "Perform completion of filesystem path."
+        if not path:
+            return self._listdir('.')
+        dirname, rest = os.path.split(path)
+        tmp = dirname if dirname else '.'
+        res = [os.path.join(dirname, p)
+                for p in self._listdir(tmp) if p.startswith(rest)]
+        # more than one match, or single match which does not exist (typo)
+        if len(res) > 1 or not os.path.exists(path):
+            return res
+        # resolved to a single directory, so return list of files below it
+        if os.path.isdir(path):
+            return [os.path.join(path, p) for p in self._listdir(path)]
+        # exact file match terminates this completion
+        return [path + ' ']
+
+    def complete_open(self, args):
+        "Completions for the open commands."
+        if not args:
+            return self._complete_path('.')
+        # treat the last arg as a path and complete it
+        return self._complete_path(args[-1])
+
+    def complete_save(self, args):
+        "Completions for the save commands."
+        if not args:
+            return self._complete_path('.')
+        # treat the last arg as a path and complete it
+        return self._complete_path(args[-1])
+
+    def complete(self, text, state):
+        "Generic readline completion entry point."
+        buffer = readline.get_line_buffer()
+        line = readline.get_line_buffer().split()
+        # show all commands
+        if not line:
+            return [c + ' ' for c in commands.keys()][state]
+        # account for last argument ending in a space
+        if RE_SPACE.match(buffer):
+            line.append('')
+        # resolve command to the implementation function
+        cmd = line[0].strip()
+        if cmd in commands.keys():
+            impl = getattr(self, 'complete_%s' % cmd)
+            args = line[1:]
+            if args:
+                return (impl(args) + [None])[state]
+            return [cmd + ' '][state]
+        results = [c + ' ' for c in commands.keys() if c.startswith(cmd)] + [None]
+        return results[state]
+
+
 if __name__ == '__main__':
-    print "Starting Savu Config tool"
+    print "Starting Savu Config tool (please wait for prompt)"
+
+    comp = Completer()
+    # we want to treat '/' as part of a word, so override the delimiters
+    readline.set_completer_delims(' \t\n;')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(comp.complete)
 
     # load all the packages in the plugins directory to register classes
     for loader, module_name, is_pkg in pkgutil.walk_packages(savu.plugins.__path__):
