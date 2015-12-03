145c06e2d37ddb1f9b5b911ce83a97a149a5943a
Mark Basham <mark.basham@diamond.ac.uk>
2015-12-03 21:38:02 +0000

    Small modification to use Com07 for DLS runner

--- mpi/dls/savu_launcher.sh
+++ mpi/dls/savu_launcher.sh
@@ -9,12 +9,12 @@ processfile=$2
 outpath=$3
 outname=savu
 nNodes=1
-nCPUs=20
+nCPUs=10
 
 filepath=$savupath/bin/savu_mpijob.sh
-M=$((nNodes*20))
+M=$((nNodes*12))
 
-qsub -N $outname -sync y -j y -o /dls/tmp/savu/ -e /dls/tmp/savu/ -pe openmpi $M -l infiniband -q medium.q@@com10 $filepath $savupath $datafile $processfile $outpath $nCPUs > /dls/tmp/savu/$USER.out
+qsub -N $outname -sync y -j y -o /dls/tmp/savu/ -e /dls/tmp/savu/ -pe openmpi $M -l infiniband -q medium.q@@com07 $filepath $savupath $datafile $processfile $outpath $nCPUs > /dls/tmp/savu/$USER.out
 
 echo "SAVU_LAUNCHER:: Job Complete, preparing output..."
 
