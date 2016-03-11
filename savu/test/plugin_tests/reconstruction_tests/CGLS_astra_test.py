a74bfce4610d25cf427a73341c6edec1fddfe328
Mark Basham <mark.basham@diamond.ac.uk>
2016-03-11 23:42:25 +0000

    update imports for recon tests

--- savu/test/plugin_tests/reconstruction_tests/CGLS_astra_test.py
+++ savu/test/plugin_tests/reconstruction_tests/CGLS_astra_test.py
@@ -24,7 +24,7 @@ import unittest
 import tempfile
 from savu.test import test_utils as tu
 
-from savu.test.plugin_runner_test import run_protected_plugin_runner
+from savu.test.framework_tests.plugin_runner_test import run_protected_plugin_runner
 
 
 class CglsAstraTest(unittest.TestCase):
