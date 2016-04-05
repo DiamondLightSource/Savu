993386d3bb4975d3ede1e887ea2f3c6ba2a7c2f1
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2016-04-05 13:38:53 +0100

    missed a file in the test

--- savu/test/plugin_tests/filter_tests/fitting_test.py
+++ savu/test/plugin_tests/filter_tests/fitting_test.py
@@ -31,25 +31,25 @@ from savu.test.framework_tests.plugin_runner_test import \
 
 class FittingTest(unittest.TestCase):
 
-#     def test_simple_fit_XRF(self):
-#         options = {
-#             "transport": "hdf5",
-#             "process_names": "CPU0",
-#             "data_file": tu.get_test_data_path('mm.nxs'),
-#             "process_file": tu.get_test_process_path('simple_fit_test_XRF.nxs'),
-#             "out_path": tempfile.mkdtemp()
-#             }
-#         run_protected_plugin_runner(options)
-
     def test_simple_fit_XRF(self):
         options = {
             "transport": "hdf5",
             "process_names": "CPU0",
-            "data_file": '/dls/i13/data/2015/cm12165-5/processing/AskAaron/mmbig_58905.nxs',
-            "process_file": '/dls/i13/data/2015/cm12165-5/processing/AskAaron/test/simple_fit_test_XRF.nxs',
+            "data_file": tu.get_test_data_path('mm.nxs'),
+            "process_file": tu.get_test_process_path('simple_fit_test_XRF.nxs'),
             "out_path": tempfile.mkdtemp()
             }
         run_protected_plugin_runner(options)
+
+#     def test_simple_fit_XRF(self):
+#         options = {
+#             "transport": "hdf5",
+#             "process_names": "CPU0",
+#             "data_file": '/dls/i13/data/2015/cm12165-5/processing/AskAaron/mmbig_58905.nxs',
+#             "process_file": '/dls/i13/data/2015/cm12165-5/processing/AskAaron/test/simple_fit_test_XRF.nxs',
+#             "out_path": tempfile.mkdtemp()
+#             }
+#         run_protected_plugin_runner(options)
 
 if __name__ == "__main__":
     unittest.main()
