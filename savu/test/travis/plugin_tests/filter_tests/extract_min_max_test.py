9d79d980d9d7cf89df680120bd7928dd7f6a3390
mjn19172 <nicoletta.de-maio@diamond.ac.uk>
2016-05-04 14:54:07 +0100

    adding first drafts of extract_min_max.py and its test

---
+++ savu/test/travis/plugin_tests/filter_tests/extract_min_max_test.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+.. module:: extract_min_max_test
+   :platform: Unix
+   :synopsis: unittest test classes for extract_min_max plugin
+
+.. moduleauthor:: Nicoletta De Maio <scientificsoftware@diamond.ac.uk>
+
+"""
+
+import unittest
+
+import savu.test.test_utils as tu
+from savu.test.travis.framework_tests.plugin_runner_test import \
+    run_protected_plugin_runner_no_process_list
+
+
+class ExtractMinMaxTest(unittest.TestCase):
+
+    def test_extract_min_max(self):
+        options = tu.set_experiment('tomo')
+        plugin = 'savu.plugins.filters.extract_min_max'
+        run_protected_plugin_runner_no_process_list(options, plugin)
+
+if __name__ == "__main__":
+    unittest.main()
