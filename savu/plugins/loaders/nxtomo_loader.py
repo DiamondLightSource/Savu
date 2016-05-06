09070b188a0983270c1b0e1b185d812c21b1270c
nicwade <nicolawadeson@gmail.com>
2016-05-06 14:28:25 +0100

    Changes 3d_to_4d loader default value to boolean

--- savu/plugins/loaders/nxtomo_loader.py
+++ savu/plugins/loaders/nxtomo_loader.py
@@ -42,7 +42,7 @@ class NxtomoLoader(BaseLoader):
     :param flat: Optional Path to the flat field data file and path to data \
         in nxs file. Default: [None, None, 1].
     :param angles: A python statement to be evaluated or a file. Default: None.
-    :param 3d_to_4d: Set to true if this reshape is required. Default: None.
+    :param 3d_to_4d: Set to true if this reshape is required. Default: False.
     """
 
     def __init__(self, name='NxtomoLoader'):
