4e10a17cb2647708695a1056074d7ee56720f178
nicwade <nicolawadeson@gmail.com>
2016-06-24 10:38:59 +0100

    Fixes base astra error

--- bin/savu_multi_qsub.sh
+++ bin/savu_multi_qsub.sh
@@ -21,9 +21,11 @@ while read -r a b c d; do
 	runfile=/bin/savu_launcher.sh
 	outpath=$PWD #outputting to the current folder
     
-    datafile=/dls/i13/data/2016/mt14080-1/raw/77391.nxs
-    processfile=$outpath/test.nxs
+    #datafile=/dls/i13/data/2016/mt14080-1/raw/77391.nxs
+    #processfile=$outpath/test.nxs
 
+    datafile=/dls/i18/data/2016/sp12778-1/processing/Savu_testing_steve/XRF/67086_homer_absorption_tomo17kev_1.nxs
+    processfile=$outpath/i18_test2.nxs
 
     #datafile=/dls/i12/data/2016/cm14465-1/processing/brick-data/dls/i12/data/2014/cm4963-3/rawdata/40384.nxs
     #processfile=/home/qmm55171/Documents/Git/git_repos/Savu/scripts/config_generator/test_loader.nxs
--- savu/plugins/base_recon.py
+++ savu/plugins/base_recon.py
@@ -75,10 +75,11 @@ class BaseRecon(Plugin):
         pad_len = shape[self.pad_dim] if self.parameters['sino_pad'] else 0
         self.sino_pad = int(math.ceil((math.sqrt(2)-1)*pad_len))
 
-        bases = [b.__name__ for b in self.__class__.__bases__]
+#        bases = [b.__name__ for b in self.__class__.__bases__]
         # pad the data now if the recon is not astra
-        self.sino_func, self.cor_func = self.set_function(False) if \
-            'NewBaseAstraRecon' in bases else self.set_function(shape)
+#        self.sino_func, self.cor_func = self.set_function(False) if \
+#            'NewBaseAstraRecon' in bases else self.set_function(shape)
+        self.sino_func, self.cor_func = self.set_function(False)
 
     def set_centre_of_rotation(self, inData, mData, pData):
         try:
@@ -114,6 +115,7 @@ class BaseRecon(Plugin):
         """
         cor = self.cor[slice_list[0][self.main_dir]]
         init = data[1] if len(data) is 2 else None
+        print "initial data shape", data[0].shape
         result = self.reconstruct(self.sino_func(data[0]), self.cor_func(cor),
                                   self.angles, self.vol_shape, init)
         return result
