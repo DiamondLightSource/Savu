ad1afed385d44038616c664720624429138b9269
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2016-07-06 14:36:01 +0100

    fixed two jenkins tests by fixing the exceptions in mmloader and allowing for the fact that NXmonitor does not have axis labels

--- savu/plugins/loaders/mm_loader.py
+++ savu/plugins/loaders/mm_loader.py
@@ -46,26 +46,37 @@ class MmLoader(BaseLoader):
 
     def setup(self):
         new_dict = self.amend_dictionary()
-#         try:
-        self.setup_loader(NxfluoLoader(), new_dict)
-        logging.debug('This file contains an NXfluo')
-#         except:
-        logging.warn('This file does not contain an NXfluo')
-#         try:
-        self.setup_loader(NxxrdLoader(), self.parameters)
-        logging.debug('This file contains an NXxrd')
-#         except:
-        logging.warn('This file does not contain an NXxrd')
-#         try:/
-        self.setup_loader(NxstxmLoader(), new_dict)
-        logging.debug('This file contains an NXstxm')
-#         except:
-        logging.warn('This file does not contain an NXstxm')
-#         try:
-        self.setup_loader(NxmonitorLoader(), new_dict)
-        logging.debug('This file contains an NXmonitor')
-#         except:
-        logging.warn('This file does not contain an NXmonitor')
+        try:
+            self.setup_loader(NxfluoLoader(), new_dict)
+            logging.debug('This file contains an NXfluo')
+        except IndexError:
+            logging.warn('This file does not contain an NXfluo')
+        except:
+            raise
+        
+        try:
+            self.setup_loader(NxxrdLoader(), self.parameters)
+            logging.debug('This file contains an NXxrd')
+        except IndexError:
+            logging.warn('This file does not contain an NXxrd')
+        except:
+            raise
+        
+        try:
+            self.setup_loader(NxstxmLoader(), new_dict)
+            logging.debug('This file contains an NXstxm')
+        except IndexError:
+            logging.warn('This file does not contain an NXstxm')
+        except:
+            raise
+
+        try:
+            self.setup_loader(NxmonitorLoader(), new_dict)
+            logging.debug('This file contains an NXmonitor')
+        except IndexError:
+            logging.warn('This file does not contain an NXmonitor')
+        except:
+            raise
 
     def setup_loader(self, loader, params):
         loader._main_setup(self.exp, params)
