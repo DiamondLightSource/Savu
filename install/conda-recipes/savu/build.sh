12c3f92ea30e467c641e1cc5ab7a1b93645f456c
nicwade <nicolawadeson@gmail.com>
2016-11-04 16:20:17 +0000

    Final changes

--- install/conda-recipes/savu/build.sh
+++ install/conda-recipes/savu/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-$PYTHON setup.py install --facility $FACILITY   # Python command to install the script.
-
-# run the savu installer to install additional packages
-savu_installer.sh
+$PYTHON setup.py install --facility temp   # Python command to install the script.
 
