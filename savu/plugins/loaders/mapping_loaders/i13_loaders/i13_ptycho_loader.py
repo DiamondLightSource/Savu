e7266834717f1d55c9e9dcaea8217d8cf608b8f7
nicwade <nicolawadeson@gmail.com>
2017-02-16 10:04:59 +0000

    Restructures mapping loaders

---
+++ savu/plugins/loaders/mapping_loaders/i13_loaders/i13_ptycho_loader.py
@@ -0,0 +1,115 @@
+# Copyright 2014 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. module:: nxstxm_loader
+   :platform: Unix
+   :synopsis: A class for loading nxstxm data
+.. moduleauthor:: Nicola Wadeson <scientificsoftware@diamond.ac.uk>
+"""
+
+from savu.plugins.loaders.mapping_loaders.base_multi_modal_loader \
+    import BaseMultiModalLoader
+import numpy as np
+from savu.plugins.utils import register_plugin
+import logging
+import h5py
+
+
+@register_plugin
+class I13PtychoLoader(BaseMultiModalLoader):
+    """
+    A class to load tomography data from an NXstxm file
+    :param mono_energy: The mono energy. Default: 9.1.
+    """
+
+    def __init__(self, name='I13PtychoLoader'):
+        super(I13PtychoLoader, self).__init__(name)
+
+    def setup(self):
+        """
+         Define the input nexus file
+        :param path: The full path of the NeXus file to load.
+        :type path: str
+        """
+
+        exp = self.exp
+        data_obj = exp.create_data_object("in_data", 'ptycho')
+        data_obj.backing_file = \
+            h5py.File(exp.meta_data.get("data_file"), 'r')
+        logging.debug("Creating file '%s' '%s'_entry",
+                      data_obj.backing_file.filename, 'ptycho')
+        
+        data_obj.data = data_obj.backing_file['entry1/instrument/merlin_sw_hdf/data']
+        data_obj.set_shape(data_obj.data.shape)
+        try:
+            control = data_obj.backing_file['/entry1/instrument/ionc_i/ionc_i'].value
+        # this is global since it is to do with the beam
+            exp.meta_data.set("control", control)
+            logging.debug('adding the ion chamber to the meta data')
+        except:
+            logging.warn('No ion chamber information. Leaving this blank')
+        
+        self.exp.meta_data.set("mono_energy", self.parameters['mono_energy'])
+        
+        labels = []
+        ### set the rotation
+        rotation_angle = None
+        try:
+            rotation_angle = data_obj.backing_file['entry1/instrument/t1_theta/t1_theta'].value
+            if rotation_angle.ndim > 1:
+                rotation_angle = rotation_angle[:, 0]
+            # axis label
+            labels.append('rotation_angle.degrees')
+            
+            data_obj.meta_data.set('rotation_angle', rotation_angle)
+        except KeyError:
+            logging.debug("Not a tomography!")
+
+        ### GET THE AXES ###
+        x = data_obj.backing_file['entry1/instrument/lab_sxy/lab_sx'].value*1e-6
+        data_obj.meta_data.set('x', x)
+        y = data_obj.backing_file['entry1/instrument/lab_sxy/lab_sxy/lab_sy'].value*1e-6
+        data_obj.meta_data.set('y', y)
+        if rotation_angle is not None:
+            pos = np.zeros((x.shape[0],2,x.shape[1]))
+            pos[:,0,:] = y
+            pos[:,1,:] = x
+        else:
+            pos = np.zeros((2,len(y)))
+            pos[0,:] = y
+            pos[1,:] = x
+        data_obj.meta_data.set('xy', pos)
+        ######
+
+        ### NOW DO THE LABELS
+        labels.append('xy.metres')
+        
+        labels.extend(['detectorX.pixel','detectorY.pixel'])
+        logging.debug('The labels are: %s',labels)
+
+        data_obj.set_axis_labels(*tuple(labels))        
+        dims = range(len(data_obj.get_shape()))
+        diff_core = (-2,-1) # it will always be this
+        diff_slice = tuple(dims[:-2])
+        logging.debug("is a diffraction")
+        logging.debug("the diffraction cores are:"+str(diff_core))
+        logging.debug("the diffraction slices are:"+str(diff_slice))
+        data_obj.add_pattern("DIFFRACTION", core_dir=diff_core,
+                             slice_dir=diff_slice)
+        
+        data_obj.add_pattern("4D_SCAN", core_dir=(1,2,3),
+                        slice_dir=(0,))
+  
+        self.set_data_reduction_params(data_obj)
---
+++ savu/plugins/loaders/mapping_loaders/i18_loaders/i18_mm_loader.py
@@ -0,0 +1,96 @@
+# Copyright 2014 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. module:: mm_loader
+   :platform: Unix
+   :synopsis: A class for loading multiple data types in a multi-modal
+       experimental setup.
+
+.. moduleauthor:: Nicola Wadeson <scientificsoftware@diamond.ac.uk>
+
+"""
+
+import logging
+from savu.plugins.loaders.base_loader import BaseLoader
+from savu.plugins.loaders.mapping_loaders.i18_loaders.\
+    base_i18_multi_modal_loader import BaseI18MultiModalLoader
+# from savu.plugins.loaders.multi_modal_loaders.i18_loaders import *
+from savu.plugins.loaders.mapping_loaders.i18_loaders.i18_xrd_loader \
+    import I18xrdLoader as xrd
+from savu.plugins.loaders.mapping_loaders.i18_loaders.i18_fluo_loader \
+    import I18fluoLoader as fluo
+from savu.plugins.loaders.mapping_loaders.i18_loaders.i18_stxm_loader \
+    import I18stxmLoader as stxm
+from savu.plugins.loaders.mapping_loaders.i18_loaders.i18_monitor_loader \
+    import I18monitorLoader as mon
+
+from savu.plugins.utils import register_plugin
+from savu.core.utils import docstring_parameter
+
+
+@register_plugin
+class I18MmLoader(BaseLoader):
+
+    def __init__(self, name='I18MmLoader'):
+        super(I18MmLoader, self).__init__(name)
+        base = BaseI18MultiModalLoader()
+        base._populate_default_parameters()
+        self.doc_string = base.__doc__
+        self.dict = base.parameters
+        self.fluo_keys = self.set_params(fluo(), 'fluo')
+        self.xrd_keys = self.set_params(xrd(), 'xrd')
+        self.stxm_keys = self.set_params(stxm(), 'stxm')
+        self.mon_keys = self.set_params(mon(), 'monitor')
+        for key, value in self.dict.iteritems():
+            self.parameters[key] = value
+
+    def set_params(self, inst, name):
+        inst._populate_default_parameters()
+        copy_keys = inst.parameters.viewkeys() - self.dict.viewkeys()
+        for key in copy_keys:
+            self.parameters[key] = inst.parameters[key]
+        return list(copy_keys)
+
+    def separate_params(self, keys):
+        all_keys = self.dict.keys() + keys
+        new_dict = {}
+        for key in all_keys:
+            new_dict[key] = self.parameters[key]
+        return new_dict
+
+    @docstring_parameter(BaseI18MultiModalLoader.__doc__, xrd.__doc__,
+                         stxm.__doc__, mon.__doc__, fluo.__doc__)
+    def _override_class_docstring(self):
+        """ {0} \n {1} \n {2} \n {3} \n {4} """
+        pass
+
+    def setup(self):
+        self._data_loader(fluo(), 'fluo', self.fluo_keys)
+        self._data_loader(xrd(), 'xrd', self.xrd_keys)
+        self._data_loader(stxm(), 'stxm', self.stxm_keys)
+        self._data_loader(mon(), 'monitor', self.mon_keys)
+
+    def _data_loader(self, inst, name, key):
+        debug_str = 'This file contains an ' + name
+        warn_str = 'This file does not contain a ' + name
+        try:
+            self.setup_loader(inst, self.separate_params(key))
+            logging.debug(debug_str)
+        except:
+            logging.warn(warn_str)
+
+    def setup_loader(self, loader, params):
+        loader._main_setup(self.exp, params)
+        loader.setup()
