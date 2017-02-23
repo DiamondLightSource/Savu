c8b01ddb420fa052a942f62160136a0c9c51f87f
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-02-23 10:25:58 +0000

    adds the new parameter in the pymca master to not same folders (bad for mpi)

--- savu/plugins/filters/pymca.py
+++ savu/plugins/filters/pymca.py
@@ -23,7 +23,7 @@
 import logging
 from savu.plugins.base_filter import BaseFilter
 from savu.plugins.driver.cpu_plugin import CpuPlugin
-from savu.plugins.utils import register_plugin,dawn_compatible
+from savu.plugins.utils import register_plugin#,dawn_compatible
 import numpy as np
 import os
 import savu.test.test_utils as tu
@@ -40,29 +40,27 @@ class Pymca(BaseFilter, CpuPlugin):
     """
 
     def __init__(self):
-        logging.debug("cropping spectrum")
+        logging.debug("fitting spectrum")
         super(Pymca, self).__init__("Pymca")
         
     def pre_process(self):
-#         slice_dir = self.get_plugin_in_datasets()[0].get_slice_dimension()
-#         print "the slice dir in the preprocess is:"+str(slice_dir)
         in_dataset, _out_datasets = self.get_datasets()
         in_d1 = in_dataset[0]
-        sh = in_d1.get_shape()
-        self.b = self.setup_fit(np.random.random((1,1,sh[-1])))
+        self.sh = in_d1.get_shape()
+        self.b = self.setup_fit(np.random.random((1,1,self.sh[-1])))
 
     def filter_frames(self, data):
-#         print np.array(data).shape
-        y=np.array(data)
         y = np.expand_dims(data,0)
-#         print "filter IN shape is:"+str(y.shape)
         self.b = self.setup_fit(y)
         self.b._McaAdvancedFitBatch__processStack()
-        stack = np.array(self.b._McaAdvancedFitBatch__images.values())
-#         print "stack shape is:",stack.shape
-        op_stack = np.rollaxis(stack,0,3)
-#         print "filter OUT shape is"+str(op_stack.shape)
-        return op_stack[0]# savu expects us to output a rank 2 here
+        try:
+            stack = np.array(self.b._McaAdvancedFitBatch__images.values())
+            op_stack = np.rollaxis(stack,0,3)
+        except AttributeError as e:
+            op_stack = -np.ones((1,1,self.outputshape[-1]))
+            logging.warn("Error in fit:%s",e) 
+        op = op_stack[0,0]
+        return op
 
     def setup(self):
         logging.debug('setting up the pymca fitting')
@@ -70,21 +68,26 @@ class Pymca(BaseFilter, CpuPlugin):
         inshape = in_dataset[0].get_shape()
         spectra_shape = inshape[-1] #  nearly always true.
         rest_shape = inshape[:-1]
-        c = self.setup_fit(np.random.random((1,1,spectra_shape)))# seed it with junk, zeros made the matrix singular unsurprisingly and this bungles it.
+        dummy_spectrum = np.random.random((1, 1, spectra_shape))
+        c = self.setup_fit(dummy_spectrum)# seed it with junk, zeros made the matrix singular unsurprisingly and this bungles it.
+        
         c.processList()#_McaAdvancedFitBatch__processStack()# perform an initial fit to get the shapes
         fit_labels = c._McaAdvancedFitBatch__images.keys() # and then take out the axis labels for the channels
-        outputshape = rest_shape+(len(fit_labels),) # and this is the shape the thing will be
+        out_meta_data = out_datasets[0].meta_data
+        out_meta_data.set_meta_data("PeakElements",fit_labels)
+        print fit_labels
+        self.outputshape = rest_shape+(len(fit_labels),) # and this is the shape the thing will be
 #         print "input shape is", in_dataset[0].get_shape()
 #         print "the output shape in setup is"+str(outputshape)
         
-        axis_labels = ['-1.FitAreas.unit']
+        axis_labels = ['-1.PeakElements.label']
         pattern_list = ['SINOGRAM', 'PROJECTION']
         fitResult = out_datasets[0]
 
         fitResult.create_dataset(patterns={in_dataset[0]: pattern_list},
                                 axis_labels={in_dataset[0]: axis_labels},
-                                shape=outputshape)
-        fitResult.meta_data.set_meta_data('FitAreas',fit_labels)
+                                shape=self.outputshape)
+        fitResult.meta_data.set_meta_data('FitAreas',np.array(fit_labels))
         slice_directions = tuple(range(len(rest_shape)))
 #         print "slice directions are:"+str(slice_directions)
         fitResult.add_pattern("CHANNEL", core_dir=(-1,),
@@ -115,13 +118,13 @@ class Pymca(BaseFilter, CpuPlugin):
         outputdir=None # nope
         roifit=0# nope
         roiwidth=y.shape[1] #need this to pretend its an image
-
         b = McaAdvancedFitBatch.McaAdvancedFitBatch(self.get_conf_path(),
                                                     [y],
                                                     outputdir,
                                                     roifit,
                                                     roiwidth,
-                                                    fitfiles=0) # prime the beauty
+                                                    fitfiles=0,
+                                                    nosave=True) # prime the beauty
         b.pleaseBreak = 1
         b.processList()
         b.pleaseBreak = 0
