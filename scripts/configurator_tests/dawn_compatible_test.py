da6dc5f0797d53f45a0d919b5a8f7496a7bb15ef
Aaron Parsons <aaron.parsons@diamond.ac.uk>
2017-03-07 13:21:15 +0000

    added the configurator test, and uncommented the rest of the dawn tests

--- scripts/configurator_tests/dawn_compatible_test.py
+++ scripts/configurator_tests/dawn_compatible_test.py
@@ -10,25 +10,25 @@ class DawnCompatibleTest(unittest.TestCase):
     def test_populate_plugins(self):
         from savu.plugins import utils as pu
         pu.populate_plugins()
-        pu.dawn_plugins = {}# reset this after the test
-        pu.dawn_plugin_params = {} # reset this after the test
-        pu.plugins = {}
-        pu.plugins_path = {}
-        pu.count = 0  
-#     def test_dawn_plugins_found(self):
-#         pu.populate_plugins()
-#         self.assertGreater(len(pu.dawn_plugins), 0)
-#   
-#     def test_dawn_plugin_params_found(self):
-#         pu.populate_plugins()
-#         self.assertTrue(isinstance(pu.dawn_plugin_params[pu.dawn_plugins.keys()[0]], dict))
-#  
-#     def test_load_plugin(self):
-#         pu.populate_plugins()
-#         plugin_path = pu.dawn_plugins[dawn_plugins.keys()[0]]['path2plugin'].split('.py')[0]
-#         inst = pu.load_plugin(plugin_path)
-#         sl = inst.__dict__['slice_list']
-#         exp = inst.__dict__['exp']
+
+    def test_dawn_plugins_found(self):
+        from savu.plugins import utils as pu
+        pu.populate_plugins()
+        self.assertGreater(len(pu.dawn_plugins), 0)
+   
+    def test_dawn_plugin_params_found(self):
+        from savu.plugins import utils as pu
+        pu.populate_plugins()
+        self.assertTrue(isinstance(pu.dawn_plugin_params[pu.dawn_plugins.keys()[0]], dict))
+  
+    def test_load_plugin(self):
+        from savu.plugins import utils as pu
+        pu.populate_plugins()
+        from savu.plugins.utils import dawn_plugins
+        plugin_path = pu.dawn_plugins[dawn_plugins.keys()[0]]['path2plugin'].split('.py')[0]
+        inst = pu.load_plugin(plugin_path)
+        sl = inst.__dict__['slice_list']
+        exp = inst.__dict__['exp']
 
 if __name__=='__main__':
     unittest.main()
