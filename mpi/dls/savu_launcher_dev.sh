955f4bab37cb568d5d7ca2a950bb58beff7c36e9
nicwade <nicolawadeson@gmail.com>
2017-03-09 10:57:59 +0000

    Updates savu_launcher_dev script

--- mpi/dls/savu_launcher_dev.sh
+++ mpi/dls/savu_launcher_dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-module load global/cluster
 
 count=0
 while read -r entry; do
@@ -12,6 +11,7 @@ done < $1
 version=${var[0]}
 echo "module loading "$version
 module load $version
+module load global/cluster
 
 cluster=medium.q@@${var[1]}
 nodes=${var[2]}
@@ -30,40 +30,100 @@ echo "Input file: $input_file"
 echo "Process list: $process_file"
 echo -e "*******************************************************************************\n"
 
-DIR="$(cd "$(dirname "$0")" && pwd)"
-file_path=$DIR'/savu_mpijob_dev.sh'
-savu_path=$(python -c "import savu, os; print os.path.dirname(os.path.abspath(savu.__file__))")
-savu_path=${savu_path%/savu}
 
-# set the log path
-log_path=/dls/tmp/savu
-a=( $options )
-for (( i=0; i<${#a[@]} ; i+=2 )) ; do
-    if [ ${a[i]} == "-l" ]; then
-        log_path=${a[i+1]}
+# function for parsing optional arguments
+function arg_parse ()
+{
+  flag=$1
+  return=$2
+  while [[ $# -gt 3 ]] ; do
+    if [ $3 == $flag ] ; then
+      eval "$return"=$4
     fi
-done
+    shift
+  done
+}
 
-qsub -N $outname -sync y -j y -o $log_path -e $log_path -pe openmpi $processes \
-     -l exclusive -l infiniband -l gpu=1 -q $cluster $file_path $version $savu_path \
-     $input_file $process_file $output_folder $cpus_per_node \
-     $gpus_per_node $options -s cs04r-sc-serv-14 > /dls/tmp/savu/$USER.out
+if [[ $@ == '--help' ]] ; then
+    echo -e "\n\t************************* SAVU HELP MESSAGE ****************************"
+    tput setaf 6
+    echo -e "\n\t To submit a Savu parallel job to the cluster, please follow the "
+    echo -e "\t template below:"
+    tput sgr0
+    echo -e "\n\t >>> savu_mpi  <in_file>  <process_list>  <out_folder>  <optional_args>"
+    tput setaf 6
+    echo -e "\n\t For a list of optional arguments type:"
+    tput sgr0
+    echo -e "\t >>> savu --help"
+    tput setaf 6
+    echo -e "\n\t It is recommended that you pass the optional arg '-d <temp_dir>', "
+    echo -e "\t where temp_dir is the temporary directory for your visit, if you are "
+    echo -e "\t running Savu on a full dataset. Ask your local contact for help."
+    tput sgr0
+    echo -e "\n\t\t\t *** THANK YOU FOR USING SAVU! ***"
+    echo -e "\n\t************************************************************************\n"
+    tput sgr0
+    exit    
+fi
 
-echo "SAVU_LAUNCHER:: Job Complete, preparing output..."
 
+# get the Savu path
+DIR="$(cd "$(dirname "$0")" && pwd)"
+filepath=`command -v savu_mpijob.sh`
+savupath=$(python -c "import savu, os; print savu.__path__[0]")
+savupath=${savupath%/savu}
+
+
+# set the output folder
+arg_parse "-f" foldername "$@"
+if [ ! $foldername ] ; then
+  IFS=. read path ext <<<"${input_file##*-}"
+  foldername=$(date +%Y%m%d%H%M%S)"_$(basename $path)"
+fi
+outfolder=$output_folder/$foldername
+# check the output folder exists - error if not
+
+# create the output folder
+if [ ! -d $outfolder ]; then
+  echo -e "\t Creating the output folder "$outfolder
+  mkdir -p $outfolder;
+fi
+# create the user log
+touch $outfolder/user.log
+
+# set the intermediate folder
+arg_parse "-d" interfolder "$@"
+if [ ! $interfolder ] ; then
+  interfolder=$outfolder
+fi
+
+
+qsub -N $outname -j y -o $interfolder -e $interfolder -pe openmpi $processes -l exclusive \
+     -l infiniband -l gpu=2 $filepath $savupath $input_file \
+     $process_file $output_folder $cpus_per_node $gpus_per_node $options -c \
+     -f $outfolder -s cs04r-sc-serv-14 -l $outfolder > /dls/tmp/savu/$USER.out
+
+# get the job number here
 filename=`echo $outname.o`
 jobnumber=`awk '{print $3}' /dls/tmp/savu/$USER.out | head -n 1`
-filename=$log_path/$filename$jobnumber
 
-while [ ! -f $filename ]
-do
-  sleep 2
-done
-
-echo "SAVU_LAUNCHER:: Output ready, spooling now"
-
-cat $filename
-
-echo "SAVU_LAUNCHER:: Process complete"
-exit
+echo -e "\n\t************************************************************************"
+echo -e "\n\t\t\t *** THANK YOU FOR USING SAVU! ***"
+tput setaf 6
+echo -e "\n\t Your job has been submitted to the cluster with job number "$jobnumber"."
+tput setaf 3
+echo -e "\n\t\t* Monitor the status of your job on the cluster:"
+tput sgr0
+echo -e "\t\t   >> module load global/cluster"
+echo -e "\t\t   >> qstat"
+tput setaf 3
+echo -e "\n\t\t* Monitor the progression of your Savu job:"
+tput sgr0
+echo -e "\t\t   >> tail -f $outfolder/user.log"
+echo -e "\t\t   >> Ctrl+C (to quit)"
+tput setaf 6
+echo -e "\n\t For a more detailed log file see: "
+echo -e "\t   $interfolder/savu.o$jobnumber"
+tput sgr0
+echo -e "\n\t************************************************************************\n"
 
