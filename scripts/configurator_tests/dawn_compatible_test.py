8a882847a3f0936e2a51ec69cdfd975a8fc31e12
nicwade <nicolawadeson@gmail.com>
2017-03-22 09:47:09 +0000

    Fixes dawn compatible tests due to plugin utils restructuring

--- scripts/configurator_tests/dawn_compatible_test.py
+++ scripts/configurator_tests/dawn_compatible_test.py
@@ -1,34 +1,35 @@
 '''
-This tests the functions required by dawn. If these test break, the dawn integration will certainly fail.
+This tests the functions required by dawn. If these tests break, the dawn \
+integration will certainly fail.
 '''
 
 import unittest
+import scripts.config_generator.config_utils as cu
+import savu.plugins.utils as pu
+from savu.plugins.utils import dawn_plugins
 
 
 class DawnCompatibleTest(unittest.TestCase):
-    
+
     def test_populate_plugins(self):
-        from savu.plugins import utils as pu
-        pu.populate_plugins()
+        cu.populate_plugins()
 
     def test_dawn_plugins_found(self):
-        from savu.plugins import utils as pu
-        pu.populate_plugins()
+        cu.populate_plugins()
         self.assertGreater(len(pu.dawn_plugins), 0)
-   
+
     def test_dawn_plugin_params_found(self):
-        from savu.plugins import utils as pu
-        pu.populate_plugins()
-        self.assertTrue(isinstance(pu.dawn_plugin_params[pu.dawn_plugins.keys()[0]], dict))
-  
+        cu.populate_plugins()
+        self.assertTrue(isinstance(
+                pu.dawn_plugin_params[pu.dawn_plugins.keys()[0]], dict))
+
     def test_load_plugin(self):
-        from savu.plugins import utils as pu
-        pu.populate_plugins()
-        from savu.plugins.utils import dawn_plugins
-        plugin_path = pu.dawn_plugins[dawn_plugins.keys()[0]]['path2plugin'].split('.py')[0]
-        inst = pu.load_plugin(plugin_path)
+        cu.populate_plugins()
+        plugin_path = pu.dawn_plugins[dawn_plugins.keys()[0]]['path2plugin']
+        inst = pu.get_plugin(plugin_path)
         sl = inst.__dict__['slice_list']
         exp = inst.__dict__['exp']
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     unittest.main()
