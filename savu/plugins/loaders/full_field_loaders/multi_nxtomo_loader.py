2a8e8c8b581eafbfed5b1ab896a726e162cfb345
nicwade <nicolawadeson@gmail.com>
2017-05-18 16:50:44 +0100

    Changes core_dir to core_dims in multi nxtomo loader

--- savu/plugins/loaders/full_field_loaders/multi_nxtomo_loader.py
+++ savu/plugins/loaders/full_field_loaders/multi_nxtomo_loader.py
@@ -118,9 +118,9 @@ class MultiNxtomoLoader(BaseLoader):
 
         data_obj.set_axis_labels(*axis_labels)
 
-        data_obj.add_pattern('PROJECTION', core_dir=(detX, detY),
+        data_obj.add_pattern('PROJECTION', core_dims=(detX, detY),
                              slice_dims=(rot, extra))
-        data_obj.add_pattern('SINOGRAM', core_dir=(detX, rot),
+        data_obj.add_pattern('SINOGRAM', core_dims=(detX, rot),
                              slice_dims=(detY, extra))
 
     def _extend_axis_label_values(self, data_obj_list, data_obj):
