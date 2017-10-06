fcf78f3d7eacfa06dcd25b9631dd17f04f02e7b6
nicwade <nicolawadeson@gmail.com>
2017-10-06 10:52:53 +0100

    Adds name field to ptycho loader parameters

--- savu/plugins/loaders/mapping_loaders/nxptycho_loader.py
+++ savu/plugins/loaders/mapping_loaders/nxptycho_loader.py
@@ -32,6 +32,7 @@ import logging
 class NxptychoLoader(BaseMultiModalLoader):
     """
     A class to load tomography data from an NXstxm file
+    :param name: The name assigned to the dataset. Default: 'ptycho'.
     """
 
     def __init__(self, name='NxptychoLoader'):
@@ -46,11 +47,12 @@ class NxptychoLoader(BaseMultiModalLoader):
         """
 
         data_str = '/instrument/detector/data'
-        data_obj, stxm_entry = self.multi_modal_setup('NXptycho', data_str)
+        data_obj, stxm_entry = self.multi_modal_setup('NXptycho', data_str,
+                                                      self.parameters['name'])
         mono_energy = data_obj.backing_file[
             stxm_entry.name + '/instrument/monochromator/energy']
         self.exp.meta_data.set("mono_energy", mono_energy)
-        
+
         labels = []
         ### set the rotation
         rotation_angle = \
