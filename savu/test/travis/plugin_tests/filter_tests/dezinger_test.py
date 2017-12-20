db35eeb05608c361ff5e5bf58c0a3882462d32e4
nicwade <nicolawadeson@gmail.com>
2017-12-20 11:19:47 +0000

    Adds a test for the new dezinger plugin

--- savu/test/travis/plugin_tests/filter_tests/dezinger_test.py
+++ savu/test/travis/plugin_tests/filter_tests/dezinger_test.py
@@ -12,29 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 """
-.. module:: plugins_test
+.. module:: dezinger_test
    :platform: Unix
-   :synopsis: unittest test classes for plugins
+   :synopsis: Tests for the dezinger plugin
 
 .. moduleauthor:: Nicola Wadeson <scientificsoftware@diamond.ac.uk>
 
 """
-
 import unittest
+from savu.test import test_utils as tu
 
-import savu.test.test_utils as tu
 from savu.test.travis.framework_tests.plugin_runner_test import \
-    run_protected_plugin_runner_no_process_list
+    run_protected_plugin_runner
 
 
-class DezingFilterTest(unittest.TestCase):
+class PaganinTest(unittest.TestCase):
 
-    def test_dezing_filter(self):
-        options = tu.set_experiment('tomoRaw')
-        plugin = 'savu.plugins.filters.dezing_filter'
-        run_protected_plugin_runner_no_process_list(options, plugin)
-
+    def test_paganin(self):
+        data_file = tu.get_test_data_path('24737.nxs')
+        process_file = tu.get_test_process_path('dezinger_test.nxs')
+        run_protected_plugin_runner(tu.set_options(data_file,
+                                                   process_file=process_file))
 if __name__ == "__main__":
     unittest.main()
