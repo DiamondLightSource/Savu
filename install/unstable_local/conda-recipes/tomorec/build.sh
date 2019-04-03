b37b981c05236ccfec77b7727746f9e29e39054a
dkazanc <dkazanc@hotmail.com>
2019-04-03 16:09:11 +0100

    removes unstable_local and updates to installation scripts

--- install/unstable_local/conda-recipes/tomorec/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-git clone git@github.com:dkazanc/TomoRec.git
-conda build TomoRec/Wrappers/Python/conda-recipe --numpy 1.15 --python 2.7
-conda install -y -q --use-local tomorec
-cd ../
-rm -r -f build
