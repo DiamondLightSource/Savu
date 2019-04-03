b501922d6b8e5acec1baba80c82900d115c37c09
dkazanc <dkazanc@hotmail.com>
2019-04-03 10:19:54 +0100

    unstable master for local and global install

---
+++ install/unstable_local/conda-recipes/h5py/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set compiler wrapper
+mpicc=$(command -v mpicc)
+mpi=`dirname $mpicc`
+
+# set anaconda distribution
+#ana_path=$(command -v savu)
+#ana_path=${ana_path%/bin/savu}
+export ana_path=/scratch/anaconda2/envs/SAVU_py27
+hdf5_version=1.10.4
+hdf5_build_no=1
+
+export LD_LIBRARY_PATH=$mpi/lib:$mpi/include:$ana_path/lib:$LD_LIBRARY_PATH
+export LD_RUN_PATH=$LD_LIBRARY_PATH
+export PYTHONPATH=$PYTHONPATH:$ana_path/lib/python2.7/site-packages
+export PATH=$PATH:$ana_path/bin/
+export CC=$mpicc
+
+$PYTHON setup.py configure --hdf5=$ana_path
+$PYTHON setup.py configure --hdf5-version=$hdf5_version
+$PYTHON setup.py configure --mpi
+$PYTHON setup.py build
+$PYTHON setup.py install
+
