57158dca9bd60b448e4703a9dbf9febd0b022948
dkazanc <dkazanc@hotmail.com>
2019-05-13 15:28:10 +0100

    adding Geant4 related bits

--- savu/plugins/simulation/tomo_geant4.py
+++ savu/plugins/simulation/tomo_geant4.py
@@ -26,9 +26,11 @@ from savu.plugins.driver.cpu_plugin import CpuPlugin
 from savu.plugins.utils import register_plugin
 
 # import *
-
-import os
 #from savu.data.plugin_list import CitationInformation
+#import os
+import sys
+sys.path.insert(0, '/home/kjy41806/Documents/SOFT/OTHER/buildgeant4/src/')
+import sim
 import numpy as np
 
 @register_plugin
@@ -54,8 +56,71 @@ class TomoGeant4(Plugin, CpuPlugin):
         self.out_dataset[0].create_dataset(patterns=in_dataset[0],
                                            axis_labels=in_dataset[0],
                                            shape=self.out_shape)
-        self.out_pData[0].plugin_data_setup('PROJECTION', 'single')       
-        
+        self.out_pData[0].plugin_data_setup('PROJECTION', 'single')
+
+        mm = 1.
+        cm = 10.*mm
+        m  = 1000.*mm
+        km = 1000.*m
+        um = 1.e-6*m
+        nm = 1.e-9*m
+        pm = 1.e-12*m
+
+        #Angle
+        rad = 1.
+        mrad = 1.e-3*rad
+        deg = (3.14159265358979323846/180.0)*rad
+
+        #Energy
+        MeV = 1.
+        eV = 1.e-6*MeV
+        keV = 1.e-3*MeV
+        GeV = 1.e3*MeV
+        TeV = 1.e6*MeV
+        PeV = 1.e9*MeV
+
+        this_directory = '/home/kjy41806/Documents/SOFT/OTHER/buildgeant4/settings/'
+        #General settings
+        verbose         = 5
+        interactive     = True
+
+        #Image variables
+        projections     = 10
+        ndarkflatfields = 1
+        particles       = 1.e3
+
+        #Rotation angles
+        startrotation   = 0.*deg
+        endrotation     = 180.*deg
+        rotation_angles = np.linspace(start    = startrotation,  \
+                                      stop     = endrotation,    \
+                                      num      = projections,    \
+                                      endpoint = False)
+        startpos = +0.*mm
+        endpos   = -0.*mm
+        zpos     = np.linspace(start    = startpos,    \
+                               stop     = endpos,      \
+                               num      = projections, \
+                               endpoint = True)
+
+        #Macro files to be included
+        physics         = this_directory + "/physics.mac"
+        materials       = this_directory + "/materials.mac"
+        detectors       = this_directory + "/detectors.mac"
+        beam            = this_directory + "/beam.mac"
+
+        #sample   = this_directory + "/samples/Al_Ni_block.mac"
+
+        sampleGeometry  = this_directory + "/sampleGeometry.mac"
+        samplePlacement = this_directory + "/samplePlacement.mac"
+
+        self.macrofiles      = [physics,
+                           materials,
+                           detectors,
+                           beam,
+                           sampleGeometry,
+                           samplePlacement]
+
         #print(self.parameters['pattern'])
 
 
@@ -72,6 +137,13 @@ class TomoGeant4(Plugin, CpuPlugin):
         # set parameters for __Geant4TomoSim__:
         self.detectors_X = self.parameters['detectors_X']
         self.detectors_Y = self.parameters['detectors_Y']
+
+        self.tomosim = sim.G4TomoSim(5, True)
+        self.tomosim.addMacroFiles(self.macrofiles)
+        self.tomosim.runsingleprojection(n_particles    = 1.e5,
+                            flatfields     = False,
+                            rotation_angle = 0.,
+                            zposition      = 0.)
         #print "The full data shape is", self.get_in_datasets()[0].get_shape()
         #print "Example is", self.parameters['example']
     def process_frames(self, data):
@@ -81,9 +153,12 @@ class TomoGeant4(Plugin, CpuPlugin):
         # generate angles
         #self.angles = np.linspace(0.0,179.9,self.parameters['proj_num'],dtype='float32')
         #self.out_dataset[0].meta_data.set('rotation_angle', self.angles)
+        #projdata = np.ones([self.detectors_Y, self.detectors_X])
+        #Length
+
         projdata = np.ones([self.detectors_Y, self.detectors_X])
         return projdata
-    
+
     def __set_image_key(self, data_obj):
         proj_slice = \
             data_obj.get_data_patterns()['PROJECTION']['slice_dims'][0]
