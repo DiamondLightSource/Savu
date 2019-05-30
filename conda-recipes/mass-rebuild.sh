942efd7c868d8860b25d85d02d9c730d555dda92
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-05-30 11:36:11 +0100

    SCI-8694: create openmpi conda package

--- conda-recipes/mass-rebuild.sh
+++ conda-recipes/mass-rebuild.sh
@@ -2,6 +2,33 @@
 
 set -e -x
 
+module purge
+module load python/3.7
+
+# our conda interpreter
+CONDA=$(which conda)
+
+# sregistry setup
+# AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are necessary for this to work...
+# come and ask me for them, export them in your shell, but never commit them!
+export SREGISTRY_S3_BUCKET=singularity-savu
+export SREGISTRY_DATABASE=/dls/tmp/$USER/singularity
+export SREGISTRY_STORAGE=/dls/tmp/$USER/singularity/shub
+
+# singularity
+export SINGULARITY_CACHEDIR=/scratch/singularity
+export SINGULARITY_TMPDIR=/scratch/tmp
+
+# pull our centos6 conda build image in
+sregistry pull s3://centos6/conda-build
+CENTOS6_CONDA_BUILD=$(sregistry get centos6/conda-build)
+
+SINGULARITY_EXEC="singularity exec -B /scratch,/dls_sw/apps"
+
 # FFTW -> https://jira.diamond.ac.uk/browse/SCI-8695
-conda build --user savu-dep fftw
+$SINGULARITY_EXEC $CENTOS6_CONDA_BUILD $CONDA build --user savu-dep fftw
+
+# OpenMPI -> https://jira.diamond.ac.uk/browse/SCI-8694
+$SINGULARITY_EXEC $CENTOS6_CONDA_BUILD $CONDA build --user savu-dep openmpi
+
 
---
+++ conda-recipes/openmpi/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# unset unused old fortran flags
+unset F90 F77
+
+# remove --as-needed, which causes problems for downstream builds,
+# seen in failures in petsc, slepc, and hdf5 at least
+export LDFLAGS="${LDFLAGS/-Wl,--as-needed/}"
+
+# this might not be needed?
+export FCFLAGS="$FFLAGS"
+
+#export CC=/usr/bin/gcc
+#export CXX=/usr/bin/g++
+#export FC=/usr/bin/gfortran
+#export LD=/usr/bin/ld
+
+# avoid absolute-paths in compilers
+#export CC=$(basename "$CC")
+#export CXX=$(basename "$CXX")
+#export FC=$(basename "$FC")
+
+if [ $(uname) == Darwin ]; then
+    if [[ ! -z "$CONDA_BUILD_SYSROOT" ]]; then
+        export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+        export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+    fi
+    export LDFLAGS="$LDFLAGS -Wl,-rpath,$PREFIX/lib"
+fi
+
+export LIBRARY_PATH="$PREFIX/lib"
+
+#export CPPFLAGS="$CPPFLAGS -I/usr/include"
+#export LIBS="$LIBS -L/lib64 -libverbs"
+export LDFLAGS="$LDFLAGS -Wl,-rpath-link -Wl,/lib64 -Wl,-rpath,/lib64"
+
+./configure --prefix=$PREFIX \
+            --disable-dependency-tracking \
+            --with-wrapper-cflags="-I$PREFIX/include" \
+            --with-wrapper-cxxflags="-I$PREFIX/include" \
+            --with-wrapper-fflags="-I$PREFIX/include" \
+            --with-wrapper-fcflags="-I$PREFIX/include" \
+            --with-wrapper-ldflags="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" \
+            --disable-wrapper-rpath \
+            --disable-wrapper-runpath \
+            --enable-orterun-prefix-by-default \
+            --enable-mpirun-prefix-by-default \
+            --with-verbs=/usr \
+            --with-verbs-libdir=/usr/lib64 \
+	    --disable-mpi-cxx \
+	    --disable-mpi-fortran \
+            --with-sge
+
+make -j"${CPU_COUNT:-1}"
+make install
