cb6007f8db48bb767469e1588555de099a22ee7c
dkazanc <dkazanc@hotmail.com>
2019-07-03 10:35:57 +0100

    3d updates

--- savu/plugins/reconstructions/tigre_recon.py
+++ savu/plugins/reconstructions/tigre_recon.py
@@ -36,7 +36,7 @@ class TigreRecon(BaseRecon, GpuPlugin):
     """
     A Plugin to reconstruct full-field tomographic projection data using ierative algorithms from \
     the TIGRE package.
-    
+
     :param recon_method: Reconstruction methods to choose: sart,sirt,ossart,ossart_tv, \
     asd_pocs,awasd_pocs, fbp,cgls, fista, ista. Default: 'cgls'.
     :param iterations: Number of iterations. Default: 20.
@@ -79,6 +79,82 @@ class TigreRecon(BaseRecon, GpuPlugin):
                                              (centre_of_rotation_shift, 0))
         return result
 
+    # def _get_output_size(self, in_data):
+    #     sizeX = self.parameters['output_size']
+    #     shape = in_data.get_shape()
+    #     if sizeX == 'auto':
+    #         detX = in_data.get_data_dimension_by_axis_label('detector_x')
+    #         sizeX = shape[detX]
+    #     detY = in_data.get_data_dimension_by_axis_label('detector_y')
+    #     sizeY = shape[detY]
+    #     return (sizeX,sizeY)
+
+    # def setup(self):
+    #     in_dataset, out_dataset = self.get_datasets()
+    #     # reduce the data as per data_subset parameter
+    #     self.preview_flag = \
+    #         self.set_preview(in_dataset[0], self.parameters['preview'])
+    #
+    #     axis_labels = in_dataset[0].data_info.get('axis_labels')[0]
+    #
+    #     dim_volX, dim_volY, dim_volZ = \
+    #         self.map_volume_dimensions(in_dataset[0])
+    #
+    #     axis_labels = {in_dataset[0]:
+    #                    [str(dim_volX) + '.voxel_x.voxels',
+    #                     str(dim_volY) + '.voxel_y.voxels',
+    #                     str(dim_volZ) + '.voxel_z.voxels']}
+    #     # specify reconstructed volume dimensions
+    #     (self.output_size, self.Vert_det) = self._get_output_size(in_dataset[0])
+    #     shape = [0]*len(in_dataset[0].get_shape())
+    #     shape[0] = self.output_size
+    #     shape[1] = self.Vert_det
+    #     shape[2] = self.output_size
+    #
+    #     # if there are only 3 dimensions then add a fourth for slicing
+    #     if len(shape) == 3:
+    #         axis_labels = [0]*4
+    #         axis_labels[dim_volX] = 'voxel_x.voxels'
+    #         axis_labels[dim_volY] = 'voxel_y.voxels'
+    #         axis_labels[dim_volZ] = 'voxel_z.voxels'
+    #         axis_labels[3] = 'scan.number'
+    #         shape.append(1)
+    #
+    #     if self.parameters['vol_shape'] == 'fixed':
+    #         shape[dim_volX] = shape[dim_volZ]
+    #     else:
+    #         shape[dim_volX] = self.parameters['vol_shape']
+    #         shape[dim_volZ] = self.parameters['vol_shape']
+    #
+    #     if 'resolution' in self.parameters.keys():
+    #         shape[dim_volX] /= self.parameters['resolution']
+    #         shape[dim_volZ] /= self.parameters['resolution']
+    #
+    #     out_dataset[0].create_dataset(axis_labels=axis_labels,
+    #                                   shape=tuple(shape))
+    #     out_dataset[0].add_volume_patterns(dim_volX, dim_volY, dim_volZ)
+    #
+    #     ndims = range(len(shape))
+    #     core_dims = (dim_volX, dim_volY, dim_volZ)
+    #     slice_dims = tuple(set(ndims).difference(set(core_dims)))
+    #     out_dataset[0].add_pattern(
+    #             'VOLUME_3D', core_dims=core_dims, slice_dims=slice_dims)
+    #
+    #     # set information relating to the plugin data
+    #     in_pData, out_pData = self.get_plugin_datasets()
+    #
+    #     dim = in_dataset[0].get_data_dimension_by_axis_label('rotation_angle')
+    #     nSlices = in_dataset[0].get_shape()[dim]
+    #
+    #     #in_pData[0].plugin_data_setup('PROJECTION', nSlices,
+    #     #        slice_axis='rotation_angle') # TypeError: plugin_data_setup() got an unexpected keyword argument 'slice_axis'
+    #     in_pData[0].plugin_data_setup('PROJECTION', nSlices) # CORRECT ?
+    #
+    #     # in_pData[1].plugin_data_setup('PROJECTION', nSlices) # (for PWLS)
+    #
+    #     # set pattern_name and nframes to process for all datasets
+    #     out_pData[0].plugin_data_setup('VOLUME_3D', 'single')
+
     def pre_process(self):
         # extract given parameters and make them 'self'
         self.recon_method = self.parameters['recon_method']
@@ -91,24 +167,30 @@ class TigreRecon(BaseRecon, GpuPlugin):
                                  hyper=np.float32(self.parameters['fista_converg_const']),
                                  regularisation=self.parameters['regularisation_method'],
                                  sup_kw_warning =False)
+        in_pData = self.get_plugin_in_datasets()
+        self.dimX = in_pData[0].get_data_dimension_by_axis_label('detector_x')
+        self.dimY = in_pData[0].get_data_dimension_by_axis_label('detector_y')
+
     def process_frames(self, data):
         centre_of_rotations, angles, self.vol_shape, init = self.get_frame_params()
-        sino = data[0].astype(np.float32)  # get a 2D sinogram
-        anglesTot, self.DetectorsDimH = np.shape(sino)  # get dimensions out of it
+        proj3d = data[0].astype(np.float32)  # get 3d projection data
+        #anglesTot, self.DetectorsDimH = np.shape(sinos)  # get dimensions out of it
         self.anglesRAD = np.deg2rad(angles.astype(np.float32))  # convert to radians
+        anglesTot = len(angles)
+        print(np.shape(proj3d))
 
-        # Reconstruct 2D data (sinogram) with TIGRE
-        geo = tigre.geometry(mode='parallel', nVoxel=np.array([1, self.vol_shape[0], self.vol_shape[1]]), default=True)
-        geo.nDetector = np.array([1, self.DetectorsDimH])
+        # Reconstruct 3D proj data with TIGRE
+        geo = tigre.geometry(mode='parallel', nVoxel=np.array([self.dimY, self.vol_shape[0], self.vol_shape[1]]), default=True)
+        geo.nDetector = np.array([self.dimY, self.dimX])
         geo.sDetector = geo.nDetector
-        __sino__ = np.float32(np.expand_dims(sino, axis=1))
+        #__sino__ = np.float32(np.expand_dims(sino, axis=1))
         kwargs = self.tigre_kwargs
-        recon = getattr(algs, self.recon_method)(__sino__, geo, self.anglesRAD, self.iterations, **kwargs)
+        recon = getattr(algs, self.recon_method)(proj3d, geo, self.anglesRAD, self.iterations, **kwargs)
         return np.swapaxes(recon, 0, 1)
 
     def get_max_frames(self):
-        return 'single'
-    
+        return 'multiple'
+
     def get_citation_information(self):
         cite_info1 = CitationInformation()
         cite_info1.name = 'citation1'
