598c39c374383655869adcd451db7cee444bb426
nicwade <nicolawadeson@gmail.com>
2019-11-18 16:06:13 +0000

    Amends mutations in configurator to check version numbers to avoid continuous replacement of DistortionCorrection plugin
    (cherry picked from commit eb2ef6ce7afbf6716804e86895d2bf8230f0ff9e)

--- scripts/config_generator/savu_config.py
+++ scripts/config_generator/savu_config.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Diamond Light Source Ltd.
+# Copyright 2014 Diamond Light Source Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-'''
-Created on 21 May 2015
+"""
+.. module:: savu_config.py
+   :platform: Unix
+   :synopsis: A command line tool for creating Savu plugin lists
 
-@author: ssg37927
-'''
+.. moduleauthor:: Nicola Wadeson <scientificsoftware@diamond.ac.uk>
+
+"""
 
 from __future__ import print_function
 
