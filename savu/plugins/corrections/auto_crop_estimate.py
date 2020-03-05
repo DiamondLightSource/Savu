c1483858c003ce2f8ec4d0d07447d76ac83ce546
dkazanc <dkazanc@hotmail.com>
2020-03-05 15:09:29 +0000

    auto_cropper plugin

---
+++ savu/plugins/corrections/auto_crop_estimate.py
@@ -0,0 +1,104 @@
+# Copyright 2019 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+.. module:: auto_crop_estimate
+   :platform: Unix
+   :synopsis: A plugin to estimate cropping values in order to crop projections automatically
+.. moduleauthor:: Daniil Kazantsev <scientificsoftware@diamond.ac.uk>
+"""
+from savu.plugins.driver.cpu_plugin import CpuPlugin
+from savu.plugins.utils import register_plugin
+from savu.plugins.plugin import Plugin
+
+# add larix autocropping module
+from larix.methods.misc import AUTOCROP
+
+#import logging
+import numpy as np
+
+@register_plugin
+class AutoCropEstimate(Plugin, CpuPlugin):
+    """
+    A plugin to estimate cropping values (indices) in order to crop projection data automatically \
+    (works well when the object of interest lies fully within the field of view). This plugin will return a \
+    metadata dataset indices2crop with estimated cropping indices to be accessed later wih plugin auto_crop_projections.
+    
+    :param threshold: A threshold to control the cropping strength . Default: 0.05.
+    :param margin_skip: Skip number of pixels around the image border . Default: 10.    
+    :param statbox_size: The size of the box to collect background statistics (mean) . Default: 20.
+    :param increase_crop: Increase crop values to ensure more accurate cropping . Default: 20.
+    :param out_datasets: 4x1 array with indices to crop . Default: ['indices2crop'].
+    """
+
+    def __init__(self):
+        super(AutoCropEstimate, self).__init__("AutoCropEstimate")
+
+    def setup(self):
+        # set up the output dataset that is created by the plugin
+        in_dataset, out_dataset = self.get_datasets()
+        # set information relating to the plugin data
+        in_pData, out_pData = self.get_plugin_datasets()
+        in_pData[0].plugin_data_setup('PROJECTION', self.get_max_frames())
+        self.orig_full_shape = in_dataset[0].get_shape() # includes flats/darks/projections
+        #print(self.orig_full_shape)
+
+        fullData = in_dataset[0]
+        slice_dirs = list(in_dataset[0].get_slice_dimensions())
+        self.new_shape = (np.prod(np.array(fullData.get_shape())[slice_dirs]), 4)
+
+        out_dataset[0].create_dataset(shape=self.new_shape ,
+                                      axis_labels=['x.pixels', 'y.pixels'],
+                                      remove=True,
+                                      transport='hdf5')
+        out_dataset[0].add_pattern("METADATA", core_dims=(1,), slice_dims=(0,))
+        out_pData[0].plugin_data_setup('METADATA', self.get_max_frames())
+
+    def process_frames(self, data):        
+        proj2D = data[0] # get 2D projection image [DetectorHoriz, DetectorVert]        
+        proj2D[proj2D > 0.0] = -np.log(proj2D[proj2D > 0.0])
+        proj2D[proj2D < 0.0] = 0.0
+        
+        pars = {'input_data' : proj2D, 			   # input grayscale image
+        	'threshold' : self.parameters['threshold'], # threshold to control cropping strength
+	        'margin_skip' : self.parameters['margin_skip'], # skip number of pixels around the image border
+        	'statbox_size' : self.parameters['statbox_size'],# the size of the box to collect background statistics (mean)
+	        'increase_crop' : self.parameters['increase_crop']} # increase crop values to ensure better cropping
+
+        indices2crop = AUTOCROP(pars['input_data'], pars['threshold'],
+                           pars['margin_skip'], pars['statbox_size'],
+                           pars['increase_crop'])
+
+        print(indices2crop)
+        return [np.array([indices2crop])]
+
+    def post_process(self):
+        in_datasets, out_datasets = self.get_datasets()
+        cropped_indices = out_datasets[0].data[...]
+
+        crop_left_horiz = np.min(cropped_indices[:,0])
+        crop_right_horiz = np.max(cropped_indices[:,1])
+        crop_up_vert = np.min(cropped_indices[:,2])
+        crop_down_vert = np.max(cropped_indices[:,3])     
+        print("Suggested values for cropping: detectorHoriz from {} to {}, detectorVertical from {} to {}".format(crop_left_horiz,crop_right_horiz,crop_up_vert,crop_down_vert))
+        in_datasets[0].meta_data.set('crop_left', crop_left_horiz)
+        print(in_datasets[0].get_name())
+        #print("Suggested values for cropping: detectorHoriz from {} to {}, detectorVertical from {} to {}".format(crop_left_horiz,crop_right_horiz,crop_up_vert,crop_down_vert))
+        #preview = [:, mid, :]
+        #self.preview_flag = self.set_preview(in_datasets[0], preview)
+
+    def nOutput_datasets(self):
+        return 1
+
+    def get_max_frames(self):
+        return 'single'
--- savu/plugins/corrections/crop_projections.py
+++ savu/plugins/corrections/crop_projections.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Diamond Light Source Ltd.
+# Copyright 2014 Diamond Light Source Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,33 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 .. module:: crop_projections
    :platform: Unix
-   :synopsis: A plugin to crop projections images without the need to specify 
+   :synopsis: A plugin to crop projections images without the need to specify
    preview dimensions.
 .. moduleauthor:: Malte Storm<malte.storm@diamond.ac.uk>
 """
+
+
+from savu.plugins.plugin import Plugin
 from savu.plugins.driver.cpu_plugin import CpuPlugin
 from savu.plugins.utils import register_plugin
-from savu.plugins.plugin import Plugin
-
-#import logging
 
 @register_plugin
 class CropProjections(Plugin, CpuPlugin):
     """
     A plugin to apply apply a crop to projection images without the need to \
     specify preview dimensions. The crop will always be applied symmetrically \
-    to the original image.    
+    to the original image.
 
     :u*param cropX: Crop in pixels applied to the x-dimensions (on each side).\
     Default: 0.
     :u*param cropY: Crop in pixels applied to the y-dimensions (on each side).\
     Default: 0.
-    :u*param useAbsoluteSizes: If True, absolute sizeX and sizeY parameters \
-    will be used to determine the new image size. If False, the image will be \
-    cropped by cropX and cropY parameters. Default: True.
+    :u*param mode: Select the mode: (absolute|relative|automatic). \
+    Absolute will use the sizeX/Y parameters to determine the final size, \
+    relative will crop the images by the amount specified in cropX/Y and \
+    automatic will use metadata to determine the cropping size. \
+    Default: 'absolute'.
     :u*param sizeX: The x-size of the cropped image in pixels. A setting of -1\
     means the original size will be preserved. Default: -1.
     :u*param sizeY: The y-size of the cropped image in pixels. A setting of -1\
@@ -47,62 +50,64 @@ class CropProjections(Plugin, CpuPlugin):
     def __init__(self):
         super(CropProjections, self).__init__("CropProjections")
 
+    def pre_process(self):
+        pass
+
+    def process_frames(self, data):
+        in_dataset, out_dataset = self.get_datasets()
+        print(in_dataset[0].meta_data.get("crop_left"))
+        return data[0][self.new_slice]
+
+    def post_process(self):
+        pass
+
     def setup(self):
         in_dataset, out_dataset = self.get_datasets()
         in_pData, out_pData = self.get_plugin_datasets()
         in_pData[0].plugin_data_setup('PROJECTION', 'single')
         det_y = in_dataset[0].get_data_dimension_by_axis_label('detector_y')
         det_x = in_dataset[0].get_data_dimension_by_axis_label('detector_x')
-        
-        self.shape = list(in_dataset[0].get_shape())
-        self.core_dims = in_pData[0].get_core_dimensions()
-        self.xcrop = self.parameters['cropX']
-        self.ycrop = self.parameters['cropY']
-        self.xsize = self.parameters['sizeX']
-        self.ysize = self.parameters['sizeY']
-        self.useAbsoluteSizes = self.parameters['useAbsoluteSizes']
 
         self.shape = list(in_dataset[0].get_shape())
         self.core_dims = in_pData[0].get_core_dimensions()
         img_dims = self.get_in_datasets()[0].get_shape()
-        
-        if self.useAbsoluteSizes:
-            if self.xsize > 0:
-                xslice = slice(self.shape[det_x] / 2 - self.xsize / 2, \
-                self.shape[det_x] / 2 + self.xsize / 2 + self.xsize % 2)
-                self.shape[det_x] = 2 * (self.xsize / 2) + self.xsize % 2
+
+        if self.parameters['mode'] == 'absolute':
+            sizeX = self.parameters['sizeX']
+            sizeY = self.parameters['sizeY']
+            if sizeX > 0:
+                xslice = slice(self.shape[det_x] / 2 - sizeX / 2,
+                self.shape[det_x] / 2 + sizeX / 2 + sizeX % 2)
+                self.shape[det_x] = 2 * (sizeX / 2) + sizeX % 2
             else:
                 xslice = slice(0, self.shape[det_x])
-            if self.ysize > 0:
-                yslice = slice(self.shape[det_y] / 2 - self.ysize / 2, \
-                self.shape[det_y] / 2 + self.ysize / 2 + self.ysize % 2)
-                self.shape[det_y] = 2 * (self.ysize / 2) + self.ysize % 2
+            if sizeY > 0:
+                yslice = slice(self.shape[det_y] / 2 - sizeY / 2,
+                self.shape[det_y] / 2 + sizeY / 2 + sizeY % 2)
+                self.shape[det_y] = 2 * (sizeY / 2) + sizeY % 2
             else:
                 yslice = slice(0, self.shape[det_y])
             self.new_slice = [yslice, xslice]
-        elif not self.useAbsoluteSizes:
-            self.new_slice = \
-                [slice(self.ycrop, img_dims[det_y] - self.ycrop), \
-                 slice(self.xcrop, img_dims[det_x] - self.xcrop)]
-            self.shape[det_x] -= 2 * self.xcrop
-            self.shape[det_y] -= 2 * self.ycrop
-        
+        elif self.parameters['mode'] == 'relative':
+            cropX = self.parameters['cropX']
+            cropY = self.parameters['cropY']
+            self.new_slice = [slice(cropY, img_dims[det_y] - cropY),
+                 slice(cropX, img_dims[det_x] - cropX)]
+            self.shape[det_x] -= 2 * cropX
+            self.shape[det_y] -= 2 * cropY
+        elif self.parameters['mode'] == 'automatic':
+            print(in_dataset[0].get_name())
+            print(in_dataset[0].meta_data.get_dictionary().keys())
+            #print in_dataset[0].meta_data.get("crop_left")
+            
+            for key, value in in_dataset[0].meta_data.get_dictionary().iteritems():
+                print (key, value)
+            #print(in_dataset[0].meta_data.get('crop_left'))
+            self.new_slice = (None, None)
+
         out_dataset[0].create_dataset(patterns=in_dataset[0],
                                       axis_labels=in_dataset[0],
                                       shape=tuple(self.shape))
         out_pData[0].plugin_data_setup('PROJECTION', 'single')
-
-    def pre_process(self):
-        pass
-
-    def process_frames(self, data):   
-        return data[0][tuple(self.new_slice)]    
-
-    def post_process(self):
-        pass
-
-    def nOutput_datasets(self):
-        return 1
-
-    def get_max_frames(self):
-        return 'single'
+        
+        print(in_dataset[0].meta_data.get("crop_left"))
