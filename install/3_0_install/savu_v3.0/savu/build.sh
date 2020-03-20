8351f321523e20f0b3acd5eed4902dbfc3bc6430
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-03-20 11:24:03 +0000

    Adds some comments

--- install/3_0_install/savu_v3.0/savu/build.sh
+++ install/3_0_install/savu_v3.0/savu/build.sh
@@ -3,6 +3,7 @@
 # activate the environment where the python build is happening
 # this will give access to the packages needed by the C source build
 source ${PREFIX%/conda-bld*}/bin/activate
+
 # build the dezing and unwarp libraries
 cd c_source_code; make; cd ..
 
