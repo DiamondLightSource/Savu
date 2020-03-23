27032e34ac1384c9564bf26d7cf83c9bc161de8e
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-03-23 13:45:26 +0000

    Removes comment and uses py3 string building

--- savu/data/data_structures/preview.py
+++ savu/data/data_structures/preview.py
@@ -79,7 +79,7 @@ class Preview(object):
                 nprocs = self.get_data_obj().exp.meta_data.get('nProcesses')
                 start = int(np.floor(nprocs/2.0))
                 end = int(np.ceil(nprocs/2.0))
-                preview_list[i] = 'mid-' + str(start) + ':mid+' + str(end)
+                preview_list[i] = f'mid-{start}:mid-{end}'
         return preview_list
 
     def _add_preview_defaults(self, plist):
@@ -93,7 +93,7 @@ class Preview(object):
         nEntries = 4
         plist = [str(i) for i in plist] if isinstance(plist[0], int) else plist
         diff_len = [(nEntries - len(elem.split(':'))) for elem in plist]
-        diff3 = [i for i in range(len(diff_len)) if diff_len[i] is 3]
+        diff3 = [i for i in range(len(diff_len)) if diff_len[i] == 3]
         for dim in diff3:
             plist[dim] = plist[dim] + ':' + plist[dim] + '+1'
             diff_len[dim] = 2
@@ -153,7 +153,7 @@ class Preview(object):
         chunks = len(preview_list)*[None]
 
         for i in range(len(preview_list)):
-            if preview_list[i] is ':':
+            if preview_list[i] == ':':
                 preview_list[i] = '0:end:1:1'
             vals = preview_list[i].split(':')
             starts[i], stops[i], steps[i], chunks[i] = \
@@ -166,9 +166,9 @@ class Preview(object):
         """
         dobj = self.get_data_obj()
         shape = dobj.get_shape()
-        mid = np.clip(np.ceil(shape[dim]/2.0).astype('int') - 1, 0, None)        
+        mid = np.clip(np.ceil(shape[dim]/2.0).astype('int') - 1, 0, None)
         end = shape[dim]
-        idx = [eval(equ) for equ in idx]
+        idx = [eval(equ, {"builtins": None}, {'mid': mid, 'end': end}) for equ in idx]
         idx = [idx[i] if idx[i] > -1 else shape[dim]+1+idx[i] for i in
                range(len(idx))]
         return idx
