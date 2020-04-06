3530acb393e91429818667eb7ad98c0c941d8119
Daniil Kazantsev <dkazanc@hotmail.com>
2020-04-06 12:08:02 +0100

    corrections section completed

--- savu/test/travis/plugin_tests/correction_tests/subpixel_shift_test.py
+++ savu/test/travis/plugin_tests/correction_tests/subpixel_shift_test.py
@@ -22,19 +22,20 @@
 """
 import unittest
 from savu.test import test_utils as tu
-
 from savu.test.travis.framework_tests.plugin_runner_test import \
     run_protected_plugin_runner
 
 
 class SubpixelShiftTest(unittest.TestCase):
+    global data_file, experiment
+    data_file = '24737.nxs'
+    experiment = None
 
     def test_subpixel_shift(self):
-        data_file = tu.get_test_data_path('24737.nxs')
-        process_file = \
-            tu.get_test_process_path('subpixel_shift.nxs')
-        run_protected_plugin_runner(tu.set_options(data_file,
-                                                   process_file=process_file))
+        process_list = 'corrections/subpixel_shift.nxs'
+        options = tu.initialise_options(data_file, experiment, process_list)
+        run_protected_plugin_runner(options)
+        tu.cleanup(options)
 
 if __name__ == "__main__":
     unittest.main()
