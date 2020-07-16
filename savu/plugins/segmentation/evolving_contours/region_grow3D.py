98a3761a2751a2575c37d67ad8ccb4a265d040e1
Daniil Kazantsev <dkazanc@hotmail.com>
2020-07-16 16:41:06 +0100

    regiongrow segmentation tests

--- savu/plugins/segmentation/evolving_contours/region_grow3D.py
+++ savu/plugins/segmentation/evolving_contours/region_grow3D.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 """
-.. module:: mask_evolve3D
+.. module:: region_grow3D
    :platform: Unix
-   :synopsis: Fast segmentation by evolving the given mask in 3D
+   :synopsis: Fast region-grow type segmentation in 3D from Larix toolbox to evolve a given mask
 
 .. moduleauthor:: Daniil Kazantsev <scientificsoftware@diamond.ac.uk>
 """
@@ -24,31 +24,30 @@ from savu.plugins.plugin import Plugin
 from savu.plugins.driver.multi_threaded_plugin import MultiThreadedPlugin
 from savu.plugins.utils import register_plugin
 
-from larix.methods.segmentation import MASK_EVOLVE
-
+from larix.methods.segmentation import REGION_GROW
 import numpy as np
 
 @register_plugin
-class MaskEvolve3d(Plugin, MultiThreadedPlugin):
+class RegionGrow3d(Plugin, MultiThreadedPlugin):
     """
-    Fast segmentation by evolving the given 3D mask, the initial mask must be given \
-    precisely through the object, otherwise segmentation will be incorrect.
+    Fast 3D segmentation by evolving the user-given mask, the initialised mask should be set \
+    in the central part of the object to be segmented.
 
-    :param threshold: important parameter to control mask propagation. Default: 1.0.
-    :param method: method to collect statistics from the mask (mean, median, value). Default: 'mean'.
-    :param iterations: The number of iterations. Default: 500.
-    :param connectivity: The connectivity of the local neighbourhood. Default: 6.
-    :param out_datasets: The default names . Default: ['MASK_EVOLVED'].
+    :param threshold: parameter to control mask propagation. Default: 1.0.
+    :param method: a method to collect statistics from the given mask (mean, median, value). Default: 'mean'.
+    :param iterations: number of iterations. Default: 500.
+    :param connectivity: the connectivity of the local neighbourhood, choose 4, 6, 8 or 26. Default: 6.
+    :param out_datasets: The default names . Default: ['MASK_RG_EVOLVED'].
     """
 
     def __init__(self):
-        super(MaskEvolve3d, self).__init__("MaskEvolve3d")
+        super(RegionGrow3d, self).__init__("RegionGrow3d")
 
     def setup(self):
         in_dataset, out_dataset = self.get_datasets()
         in_pData, out_pData = self.get_plugin_datasets()
         in_pData[0].plugin_data_setup('VOLUME_3D', 'single')
-        in_pData[1].plugin_data_setup('VOLUME_3D', 'single') # the initialisation (mask)
+        in_pData[1].plugin_data_setup('VOLUME_3D', 'single') # the initialised mask
 
         out_dataset[0].create_dataset(in_dataset[0], dtype=np.uint8)
         out_pData[0].plugin_data_setup('VOLUME_3D', 'single')
@@ -57,28 +56,29 @@ class MaskEvolve3d(Plugin, MultiThreadedPlugin):
         # extract given parameters
         self.threshold = self.parameters['threshold']
         self.iterations = self.parameters['iterations']
-        self.connectivity = self.parameters['connectivity']        
+        self.connectivity = self.parameters['connectivity']
         if (str(self.parameters['method']) == 'mean'):
             self.method = 'mean'
         elif (str(self.parameters['method']) == 'median'):
-            self.method = 'median'            
+            self.method = 'median'
         else:
             self.method = 'value'
 
     def process_frames(self, data):
         input_temp = data[0]
         indices = np.where(np.isnan(input_temp))
-        input_temp[indices] = 0.0          
-        
+        input_temp[indices] = 0.0
+
         pars = {'input_data' : input_temp, # input grayscale image
         'maskData' : np.uint8(data[1]),    # generated initialisation mask
         'threhsold' : self.threshold,      # threhsold controls where evolution stops (>=1)
         'iterationsNumb' : self.iterations,# the number of iterations (depends on the size of the phase)
         'connectivity' : self.connectivity,# voxel connectivity rule, choose between 4 (2D), 6, 8 (2D), and 26
-        'method' : self.method}            # method to collect statistics from the mask (mean. median, value)        
-        
+        'method' : self.method}            # method to collect statistics from the mask (mean. median, value)
+
+        # run the method
         if (np.sum(data[1]) > 0):
-            mask_evolve = MASK_EVOLVE(pars['input_data'], pars['maskData'],\
+            mask_evolve = REGION_GROW(pars['input_data'], pars['maskData'],\
                            pars['threhsold'], pars['iterationsNumb'],\
                            pars['connectivity'], pars['method'])
         else:
