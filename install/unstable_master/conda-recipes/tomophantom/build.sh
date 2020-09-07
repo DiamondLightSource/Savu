d89ce2e698b27e0ebf164e484b4322ecef1efa35
nicwade <nicolawadeson@gmail.com>
2020-09-07 13:39:05 +0100

    Removes obsolete installation folders

--- install/unstable_master/conda-recipes/tomophantom/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-git clone https://github.com/dkazanc/TomoPhantom.git
-export CIL_VERSION=1.2_1
-conda build TomoPhantom/Wrappers/Python/conda-recipe --numpy 1.15 --python 3.7
-conda install -y -q --use-local tomophantom
-cd ../
-rm -r -f build
