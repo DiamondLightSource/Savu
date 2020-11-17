fe694c08fd01664a361b4bc68c4dfd3812ca3730
Daniil Kazantsev <dkazanc@hotmail.com>
2020-11-17 23:34:32 +0000

    sl stop command

--- install/savu_hpc/savu_installer/check_package.sh
+++ install/savu_hpc/savu_installer/check_package.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+trap "exit" INT
 if grep -q $VER_PACKAGE check_conda_package.txt; then
     echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is found in Savu's environment, continue with installation..."
 else
     echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is NOT found in Savu's environment! \nInstallation process terminated!"
+    sl -e
     exit 0
 fi
