59fd6e2c8e4b9d05163585f57315a136aedee39d
Daniil Kazantsev <dkazanc@hotmail.com>
2020-11-18 23:46:07 +0000

    installer4

--- install/savu_hpc/savu_installer/savu_installer.sh
+++ install/savu_hpc/savu_installer/savu_installer.sh
@@ -287,24 +287,12 @@ if [ ! $test_flag ]; then
   export VER_PACKAGE=2.10.0
   conda list $PACKAGE
   conda list $PACKAGE > check_conda_package.txt
-  if grep -q $VER_PACKAGE check_conda_package.txt; then
-      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is found in Savu's environment, continue with installation..."
-  else
-      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is NOT found in Savu's environment! \nInstallation process terminated!"
-      exit 0
-  fi
-  rm -f check_conda_package.txt
+  ./check_package
 
   export PACKAGE=mpi4py
   VER_PACKAGE=$mpi4py_version
   conda list $PACKAGE > check_conda_package.txt
-  if grep -q $VER_PACKAGE check_conda_package.txt; then
-      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is found in Savu's environment, continue with installation..."
-  else
-      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is NOT found in Savu's environment! \nInstallation process terminated!"
-      exit 0
-  fi
-  rm -f check_conda_package.txt
+  ./check_package
 
   echo "Installing pytorch..."
   string=$(awk '/^cudatoolkit/' $versions_file)
