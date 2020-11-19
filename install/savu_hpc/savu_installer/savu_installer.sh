c67c4767bf5f072b81f51c92cc7b54856b79e082
Daniil Kazantsev <dkazanc@hotmail.com>
2020-11-19 17:37:17 +0000

    installer7

--- install/savu_hpc/savu_installer/savu_installer.sh
+++ install/savu_hpc/savu_installer/savu_installer.sh
@@ -315,7 +315,21 @@ if [ ! $test_flag ]; then
   string=$(awk '/^cudatoolkit/' $versions_file)
   cudatoolkit_version=$(echo $string | cut -d " " -f 2)
   conda install -y -q pytorch torchvision cudatoolkit=$cudatoolkit_version -c pytorch
-  
+
+  export PACKAGE=cudatoolkit
+  string=$(awk '/^cudatoolkit/' $versions_file)
+  cudatoolkit_version=$(echo $string | cut -d " " -f 2)
+  export VER_PACKAGE=$cudatoolkit_version
+  conda list $PACKAGE > check_conda_package.txt
+  if grep -q $VER_PACKAGE check_conda_package.txt; then
+      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is found in Savu's environment, continue with installation..."
+      rm -f check_conda_package.txt
+  else
+      echo -e "\nPackage $PACKAGE of v.$VER_PACKAGE is NOT found in Savu's environment! \nInstallation process terminated!"
+      rm -f check_conda_package.txt
+      exit 0
+  fi
+
   conda env update -n root -f $DIR/environment.yml
 
   . $recipes/installer.sh "tomophantom"
