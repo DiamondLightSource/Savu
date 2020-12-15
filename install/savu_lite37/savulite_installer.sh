447f176432f1ec2cf1f52fd6931342e685790252
Daniil Kazantsev <dkazanc@hotmail.com>
2020-12-15 23:41:41 +0000

    actions8

--- install/savu_lite37/savulite_installer.sh
+++ install/savu_lite37/savulite_installer.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 rm -rf ~/.condarc
-conda config --add channels defaults
 conda config --append channels conda-forge
 conda config --append channels astra-toolbox/label/dev
 conda config --append channels ccpi
 conda config --append channels dkazanc
 
 conda config --set anaconda_upload no
-conda config --set channel_priority strict
+conda config --set channel_priority flexible
 conda config --set auto_activate_base false
 conda config --set show_channel_urls true
 conda config --set unsatisfiable_hints true
