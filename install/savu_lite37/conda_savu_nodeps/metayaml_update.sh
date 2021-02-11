d107d70a069375e774c5300d6caff73a55f94965
Daniil Kazantsev <dkazanc@hotmail.com>
2021-02-11 11:44:51 +0000

    savu-lite auto build work

---
+++ install/savu_lite37/conda_savu_nodeps/metayaml_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#title           :metayaml_update.sh
+#description     :Thsi script updates meta.yaml for Savu-lite conda recipe based on the explicit file
+#author          :Daniil Kazantsev
+#date            :10.02.2021
+#version         :0.1
+#requirements    :meta.yaml amd savu3_0_lite_nodeps.txt
+#usage           :bash metayaml_update.sh
+
+
+meta_file='meta.yaml'
+explicit_file='spec-savu3_0_lite_nodeps.txt'
