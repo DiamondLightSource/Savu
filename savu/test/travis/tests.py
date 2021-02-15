4381f6d15c8f3a8737e86c00bc57cad1715b472a
Daniil Kazantsev <dkazanc@hotmail.com>
2021-02-15 20:51:19 +0000

    actions stopper

--- savu/test/travis/tests.py
+++ savu/test/travis/tests.py
@@ -1,12 +1,24 @@
 import os, pathlib
-import pytest
+#import pytest
+import re
+import sys
+from pytest import main
 
 savuPath = os.path.dirname(os.path.realpath(__file__))
-
-#os.chdir( savuPath  + '/framework_tests' )
-#pytest.main()
-#os.chdir( savuPath + '/plugin_tests' )
-#pytest.main()
-#os.chdir( savuPath + '/process_list_tests' )
 os.chdir( savuPath )
-pytest.main()
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
+
+"""
+exitcode = pytest.main()
+print(exitcode)
+if (exitcode == "ExitCode.TESTS_FAILED"):
+    print("NOW NOW")
+    exit(1)
+else:
+    exit(0)
+"""
+#pytest.main()
+#pytest.main(["-n", "mytestdir"])
