8a930ea5f8dfad74573de8edf3bcf71849a03daf
Daniil Kazantsev <dkazanc@hotmail.com>
2021-02-16 11:19:42 +0000

    mod tests

--- savu/test/travis/tests.py
+++ savu/test/travis/tests.py
@@ -6,8 +6,4 @@ savuPath = os.path.dirname(os.path.realpath(__file__))
 os.chdir( savuPath )
 # the tests will use the command line version of pytest.main() run
 # the parallel multicore run requires pytest-xdist software
-try:
-    sys.exit(main(["-n", "4"]))
-except:
-    print("No pytest-xdist module (parallel), running serial")
-    sys.exit(main())
+sys.exit(main(["-n", "4"]))
