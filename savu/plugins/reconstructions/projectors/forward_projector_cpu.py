33fb6c5c0c6d023a4d875b7b8abe5f4c8e7de3b6
Daniil Kazantsev <dkazanc@hotmail.com>
2021-02-17 18:29:18 +0000

    a new version of projector

---
+++ savu/plugins/reconstructions/projectors/forward_projector_cpu.py
@@ -0,0 +1,98 @@
+# Copyright 2014 Diamond Light Source Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. module:: forward_projector_cpu
+   :platform: Unix
+   :synopsis: A forward data projector using ToMoBAR software
+
+.. moduleauthor:: Daniil Kazantsev <scientificsoftware@diamond.ac.uk>
+"""
+
+from savu.plugins.plugin import Plugin
+from savu.plugins.driver.cpu_plugin import CpuPlugin
+from savu.plugins.utils import register_plugin
+
+from tomobar.methodsDIR import RecToolsDIR
+import numpy as np
+
+@register_plugin
+class ForwardProjectorCpu(Plugin, CpuPlugin):
+    """
+    This plugin uses ToMoBAR software and CPU Astra projector underneath to generate projection data.
+    The plugin will project the given object using user provided parallel-beam geometry.
+
+    :param angles_deg: Projection angles in degrees in a format [start, stop, step]. Default: [0.0, 180.0, 0.5].
+    :param det_horiz: The size of the _horizontal_ detector. Default: 300.
+    :param out_datasets: Default out dataset names. Default: ['forw_proj']
+    """
+
+    def __init__(self):
+        super(ForwardProjectorCpu, self).__init__('ForwardProjectorCpu')
+
+    def pre_process(self):
+        # set parameters
+        angles_list = self.parameters['angles_deg']
+        self.angles_rad = np.deg2rad(np.arange(angles_list[0], angles_list[1], angles_list[2], dtype=np.float))
+        self.angles_total = len(self.angles_rad)
+        self.detectors_horiz = self.parameters['det_horiz']
+
+    def setup(self):
+        in_dataset, out_dataset = self.get_datasets()
+        in_pData, out_pData = self.get_plugin_datasets()
+        in_pData[0].plugin_data_setup('VOLUME_XZ', 'single')
+
+        out_shape_sino = self.new_shape(in_dataset[0].get_shape(), in_dataset[0])
+        label = ['x.pixels', 'proj.angles', 'y.pixels']
+        pattern = {'name': 'SINOGRAM', 'slice_dims': (1,),
+                   'core_dims': (0,2)}
+        out_dataset[0].create_dataset(axis_labels=label, shape=out_shape_sino)
+        out_dataset[0].add_pattern(pattern['name'],
+                                   slice_dims=pattern['slice_dims'],
+                                   core_dims=pattern['core_dims'])
+        out_pData[0].plugin_data_setup(pattern['name'], self.get_max_frames())
+        out_dataset[0].meta_data.set('rotation_angle', self.angles_rad)
+
+    def process_frames(self, data):
+        image = data[0].astype(np.float32)
+        image = np.where(np.isfinite(image), image, 0)
+        objsize_image = np.shape(image)[0]
+        RectoolsDIR = RecToolsDIR(DetectorsDimH = self.detectors_horiz,  # DetectorsDimH # detector dimension (horizontal)
+                            DetectorsDimV = None,  # DetectorsDimV # detector dimension (vertical) for 3D case only
+                            CenterRotOffset = None, # Center of Rotation (CoR) scalar (for 3D case only)
+                            AnglesVec = self.angles_rad, # array of angles in radians
+                            ObjSize = objsize_image, # a scalar to define reconstructed object dimensions
+                            device_projector='cpu')
+        sinogram_new = RectoolsDIR.FORWPROJ(image)
+        return sinogram_new
+
+    def new_shape(self, full_shape, data):
+        # calculate a new output data shape based on the input data shape
+        core_dirs = data.get_core_dimensions()
+        angles_list = self.parameters['angles_deg']
+        self.angles_rad = np.deg2rad(np.arange(angles_list[0], angles_list[1], angles_list[2], dtype=np.float))
+        self.angles_total = len(self.angles_rad)
+        self.detectors_horiz = self.parameters['det_horiz']
+        new_shape_sino_orig = list(full_shape)
+        new_shape_sino= (self.angles_total, new_shape_sino_orig[1], self.detectors_horiz)
+        return tuple(new_shape_sino)
+
+    def get_max_frames(self):
+        return 'single'
+
+    def nInput_datasets(self):
+        return 1
+
+    def nOutput_datasets(self):
+        return 1
