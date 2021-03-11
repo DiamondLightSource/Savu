72a61bf3b7aebcf4a8b629bc639a569bc4c78b1f
nicwade <nicolawadeson@gmail.com>
2021-03-11 12:05:46 +0000

    More tools update

--- savu/plugins/base_tools.py
+++ savu/plugins/base_tools.py
@@ -7,14 +7,30 @@ class BaseTools(PluginTools):
         """
         in_datasets:
             visibility: datasets
-            dtype: list
-            description: Create a list of the dataset(s) to process
+            dtype: string_list
+            description: 
+                summary: A list of the dataset(s) to process.
+                verbose: >
+                    A list of strings, where each string gives the name of a
+                    dataset that was either specified by a loader plugin or
+                    created as output to a previous plugin.  The length of the
+                    list is the number of input datasets requested by the
+                    plugin.  If there is only one dataset and the list is left
+                    empty it will default to that dataset.
             default: []
 
         out_datasets:
             visibility: datasets
-            dtype: list
-            description: Create a list of the dataset(s) to create
+            dtype: string_list
+            description:
+                summary: A list of the dataset(s) to create.
+                verbose: >
+                    A list of strings, where each string is a name to be
+                    assigned to a dataset output by the plugin. If there is
+                    only one input dataset and one output dataset and the list
+                    is left empty, the output will take the name of the input
+                    dataset. The length of the list is the number of output
+                    datasets created by the plugin.
             default: []
 
         """
