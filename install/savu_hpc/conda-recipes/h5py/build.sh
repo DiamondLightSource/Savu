5af48a329da9fc4b94655ffb8311d5af778efe08
Daniil Kazantsev <dkazanc@hotmail.com>
2021-03-22 22:23:03 +0000

    h5py meta

--- install/savu_hpc/conda-recipes/h5py/build.sh
+++ install/savu_hpc/conda-recipes/h5py/build.sh
@@ -7,7 +7,7 @@ mpi=${mpicc%/bin/mpicc}
 # set anaconda distribution
 ana_path=$(command -v savu)
 ana_path=${ana_path%/bin/savu}
-hdf5_version=1.10.5
+hdf5_version=1.10.5_openmpi3.1.4
 hdf5_build_no=1
 
 export LD_LIBRARY_PATH=$mpi/lib:$mpi/include:$ana_path/lib:$LD_LIBRARY_PATH
@@ -23,4 +23,3 @@ $PYTHON setup.py configure --hdf5-version=$hdf5_version
 $PYTHON setup.py configure --mpi
 $PYTHON setup.py build
 $PYTHON setup.py install
-
