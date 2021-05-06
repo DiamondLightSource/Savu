c0147396e09d9083546df72b9afce2c489b0708a
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2021-05-06 09:47:02 +0100

    Add missing parameters to Multi Nxtomoloader Tools

--- savu/plugins/loaders/full_field_loaders/multi_nxtomo_loader_tools.py
+++ savu/plugins/loaders/full_field_loaders/multi_nxtomo_loader_tools.py
@@ -21,6 +21,21 @@ class MultiNxtomoLoaderTools(PluginTools):
             dtype: h5path
             description: Path to the data inside the file.
             default: 'entry1/tomo_entry/data/data'
+        dark:
+            visibility: intermediate
+            dtype: [list[filepath, h5path, int],list[None,None,int]]
+            description: Specify the nexus file location where the dark field
+              images are stored. Then specify the path within this nexus file,
+              at which the dark images are located. The last value will be a
+              scale value.
+            default: [None, None, 1]
+        flat:
+            visibility: intermediate
+            dtype: [list[filepath, h5path, int],list[None,None,int]]
+            description: This parameter needs to be specified only if flats
+              not stored in the same dataset as sample projections. Optional
+              Path to the flat field data file, nxs path and scale value.
+            default: [None, None, 1]
         stack_or_cat:
             visibility: intermediate
             dtype: str
@@ -41,5 +56,13 @@ class MultiNxtomoLoaderTools(PluginTools):
             dtype: list[int,int]
             description: The start and end of file numbers
             default: [0,10]
+        angles:
+            visibility: intermediate
+            dtype: [str, int, None]
+            description: If this is 4D data stored in 3D then pass an integer
+              value equivalent to the number of projections per 180 degree
+              scan. If the angles parameter is set to None, then values from
+              default dataset used.
+            default: None
 
         """
