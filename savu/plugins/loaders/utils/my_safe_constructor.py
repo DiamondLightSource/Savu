29d03bd55e86700fad1673123fda7d3092bb63c6
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2021-06-25 11:50:13 +0100

    Updates to yaml safe loader

--- savu/plugins/loaders/utils/my_safe_constructor.py
+++ savu/plugins/loaders/utils/my_safe_constructor.py
@@ -1,5 +1,6 @@
 from yaml.constructor import SafeConstructor
 
+
 # Create custom safe constructor class that inherits from SafeConstructor
 class MySafeConstructor(SafeConstructor):
 
@@ -8,7 +9,3 @@ class MySafeConstructor(SafeConstructor):
         return self.construct_scalar(node)
 
 
-# Inject the above boolean logic into the custom constructor
-MySafeConstructor.add_constructor(
-    "tag:yaml.org,2002:bool", MySafeConstructor.add_bool
-)
