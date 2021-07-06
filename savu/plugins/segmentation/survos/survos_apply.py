9b66fedb6d46a16ecb2c4670d940a9da221bd7ad
Mark Basham <mark.basham@diamond.ac.uk>
2021-07-06 10:32:06 +0100

    continue work on the survos apply plugin

--- savu/plugins/segmentation/survos/survos_apply.py
+++ savu/plugins/segmentation/survos/survos_apply.py
@@ -23,6 +23,15 @@ from savu.plugins.driver.gpu_plugin import GpuPlugin
 """
 
 import logging
+import sys
+
+from pathlib import Path
+
+import fastai.vision as faiv
+import numpy as np
+import torch.nn.functional as F
+from skimage import exposure, img_as_float, img_as_ubyte, io
+
 
 from savu.plugins.filters.base_filter import BaseFilter
 from savu.plugins.driver.gpu_plugin import GpuPlugin
@@ -30,13 +39,47 @@ from savu.plugins.utils import register_plugin
 from savu.data.plugin_list import CitationInformation
 
 
+# Need these in the namespace in order to load the model
+def bce_loss(logits, labels):
+    """Defines the binary cross entropy loss function used when training a U-net on binary data."""
+    logits = logits[:,1,:,:].float()
+    labels = labels.squeeze(1).float()
+    return F.binary_cross_entropy_with_logits(logits, labels)
+
+
+class BinaryLabelList(faiv.SegmentationLabelList):
+    def open(self, fn): return faiv.open_mask(fn)
+
+
+class BinaryItemList(faiv.SegmentationItemList):
+    _label_cls = BinaryLabelList
+
+
+# force them into the __main__ namespace for pickling time
+sys.modules['__main__'].__dict__['bce_loss'] = bce_loss
+sys.modules['__main__'].__dict__['BinaryItemList'] = BinaryItemList
+sys.modules['__main__'].__dict__['BinaryLabelList'] = BinaryLabelList
+
+
+def fix_odd_sides(example_image):
+    if (list(example_image.size)[0] % 2) != 0:
+        example_image = faiv.crop_pad(example_image,
+                                      size=(list(example_image.size)[0]+1,
+                                            list(example_image.size)[1]),
+                                      padding_mode = 'reflection')
+    if (list(example_image.size)[1] % 2) != 0:
+        example_image = faiv.crop_pad(example_image,
+                                      size=(list(example_image.size)[0],
+                                            list(example_image.size)[1] + 1),
+                                      padding_mode = 'reflection')
+
 @register_plugin
 class SurvosApply(BaseFilter, GpuPlugin):
     """
     A plugin to apply Paganin filter (contrast enhancement) on projections.
 
-    :u*param SegmentationConfigFile: Location of the config file to \
-        be applied. Default: "config.conf".
+    :u*param SegmentationModelFile: Location of the config file to \
+        be applied. Default: "models/unet.pkl".
     :u*param pattern: Direction to process the data (VOLUME_YZ|VOLUME_XY|VOLUME_XZ). Default: "VOLUME_YZ".
     """
 
@@ -51,12 +94,23 @@ class SurvosApply(BaseFilter, GpuPlugin):
         return self.parameters['pattern']
 
     def pre_process(self):
-        # TODO load the segmentation information
-        pass
+        model_path = Path(self.parameters['SegmentationModelFile'])
+        # Load the model
+        self.learn = faiv.load_learner(model_path.parent, model_path.name)
+        # Remove the restriction on the model prediction size
+        self.learn.data.single_ds.tfmargs['size'] = None
 
     def process_frames(self, data):
-        # TODO add code here to apply the segmentation in 4 directions, and return the sum
-        return data
+        # TODO Deal with the 4 different configurations
+        # data = img_as_float(data)
+        # Convert data to a fastai Image object
+        data_pad = data[0]
+        img = faiv.Image(faiv.pil2tensor(data_pad, data_pad.dtype))
+        fix_odd_sides(img)
+        prediction = self.learn.predict(img)
+        out = img_as_ubyte(prediction[1][0])
+        # return the unpadded size
+        return out[0:data_pad.shape[0], 0:data_pad.shape[1]]
 
     def get_max_frames(self):
         return 'single'
