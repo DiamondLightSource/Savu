482048c14937c32f3b6b150040fe45b9b06c0af5
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2021-07-23 17:29:28 +0100

    Update for the directory for documentation testing

--- doc/update_rst_1.sh
+++ doc/update_rst_1.sh
@@ -22,10 +22,10 @@ python -m doc.create_plugin_doc api_plugin plugin_autosummary.rst
 python -m doc.create_plugin_doc plugin_documentation plugin_documentation.rst
 
 # Remove the directory containing all plugin guide tests
-rm -rf $DIR/../savu/test/travis/doc_tests/plugins/
+rm -rf $DIR/doc_tests/plugins/
 
 # Remove directory for plugin guide testing logs so that there are no obsolete files
-rm -rf $DIR/../savu/test/travis/doc_tests/logs/
+rm -rf $DIR/doc_tests/logs/
 
 # Create the test file from each individual plugin guide
 # This is made by collecting the command prompt lines and process lists from each plugin guide
@@ -34,7 +34,7 @@ python -m doc.create_individual_doc_test
 # Run documentation tests for process list refresh and process list command execution
 # Log files containing the resulting output from each command will be created
 # Errors should be raised here if there is a problem with a plugin reference
-python -m unittest savu.test.travis.framework_tests.plugin_doc_test_runner
+pytest $DIR/doc_tests/plugins/
 
 module unload savu/4.0
 
