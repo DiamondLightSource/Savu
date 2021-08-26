b0c233ba8785b4a306d0662408c61af5ac00dde1
Daniil Kazantsev <dkazanc@hotmail.com>
2021-08-26 10:41:54 +0100

    conda recipies sorted

--- install/savu_hpc/conda-recipes/tomophantom/build.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ${SRC_DIR}/tomophantom
-cd tomophantom
-
-#issue cmake to create setup.py
-cmake -G "Unix Makefiles" ../ -DBUILD_PYTHON_WRAPPER=ON -DCONDA_BUILD=ON -DCMAKE_BUILD_TYPE="Release" -DLIBRARY_LIB=$CONDA_PREFIX/lib -DLIBRARY_INC=$CONDA_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
-make install
-
