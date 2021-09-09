5db19e3125d20f2747e12f1c59d7ba0aada6170b
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2021-09-09 11:31:41 +0100

    Add test files for command

--- savu/test/test_utils.py
+++ savu/test/test_utils.py
@@ -178,6 +178,7 @@ def set_options(path, **kwargs):
     options['checkpoint'] = None
     options['system_params'] = None
     options['nPlugin'] = 0
+    options['command'] = ''
     return options
 
 
