045ade6ea45e4736a293ef39cec1480362fe7a30
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2021-11-22 15:58:20 +0000

    Rename 24737 to tomo_standard in sh script

--- install/tests/mpi_gpu_test.sh
+++ install/tests/mpi_gpu_test.sh
@@ -20,5 +20,5 @@ done <<< "$IN"
 . test_setup.sh
 
 echo "Running the mpi gpu test..."
-savu_launcher.sh $version $TESTDATA/data/24737.nxs $TESTDATA/test_process_lists/mpi_gpu_test.nxs $output
+savu_launcher.sh $version $TESTDATA/data/tomo_standard.nxs $TESTDATA/test_process_lists/mpi_gpu_test.nxs $output
 
