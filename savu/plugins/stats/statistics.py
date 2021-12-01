3f61ef578343c38db99af03b6a79a051ee5ace6f
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2021-12-01 15:14:05 +0000

    Some more bugfixes

--- savu/plugins/stats/statistics.py
+++ savu/plugins/stats/statistics.py
@@ -34,28 +34,27 @@ class Statistics(object):
         cls.count = 1
         cls.data_stats = {}
         cls.volume_stats = {}
-        n_plugins = exp.meta_data.plugin_list.n_plugins
-        # for n in range(n_plugins):
-        #    cls.data_stats[n + 1] = [None, None, None, None, None]
-        #    cls.volume_stats[n + 1] = [None, None, None, None, None]
+        n_plugins = len(exp.meta_data.plugin_list.plugin_list)
+        for n in range(n_plugins):
+            cls.data_stats[n + 1] = [None, None, None, None, None]
+            cls.volume_stats[n + 1] = [None, None, None, None, None]
         cls.path = exp.meta_data['out_path']
-        print(cls.path)
         if cls.path[-1] == '/':
             cls.path = cls.path[0:-1]
-
         cls.path = f"{cls.path}/stats"
-        #if not os.path.exists(cls.path):
-        os.mkdir(cls.path)
+        if not os.path.exists(cls.path):
+            os.mkdir(cls.path)
         cls.has_setup = True
 
     def set_slice_stats(self, slice1):
-        slice_num = self.plugin.pcount
-        slice1 = self._de_list(slice1)
-        slice1 = self._unpad_slice(slice1)
-        self.stats['max'].append(slice1.max())
-        self.stats['min'].append(slice1.min())
-        self.stats['mean'].append(np.mean(slice1))
-        self.stats['standard_deviation'].append(np.std(slice1))
+        if slice1 is not None:
+            slice_num = self.plugin.pcount
+            slice1 = self._de_list(slice1)
+            slice1 = self._unpad_slice(slice1)
+            self.stats['max'].append(slice1.max())
+            self.stats['min'].append(slice1.min())
+            self.stats['mean'].append(np.mean(slice1))
+            self.stats['standard_deviation'].append(np.std(slice1))
 
     def get_slice_stats(self, stat, slice_num):
         return self.stats[stat][slice_num]
@@ -65,23 +64,28 @@ class Statistics(object):
         p_num = Statistics.count
         Statistics.data_stats[p_num] = [None, None, None, None, None]
         Statistics.volume_stats[p_num] = [None, None, None, None, None]
-        if self.pattern in ['PROJECTION', 'SINOGRAM', 'TANGENTOGRAM']:
-            Statistics.data_stats[p_num][0] = max(self.stats['max'])
-            Statistics.data_stats[p_num][1] = min(self.stats['min'])
-            Statistics.data_stats[p_num][2] = np.mean(self.stats['mean'])
-            Statistics.data_stats[p_num][3] = np.mean(self.stats['standard_deviation'])
-            Statistics.data_stats[p_num][4] = np.median(self.stats['standard_deviation'])
-        elif self.pattern in ['VOLUME_XZ', 'VOLUME_XY', 'VOLUME_YZ']:
-            Statistics.volume_stats[p_num][0] = max(self.stats['max'])
-            Statistics.volume_stats[p_num][1] = min(self.stats['min'])
-            Statistics.volume_stats[p_num][2] = np.mean(self.stats['mean'])
-            Statistics.volume_stats[p_num][3] = np.mean(self.stats['standard_deviation'])
-            Statistics.volume_stats[p_num][4] = np.median(self.stats['standard_deviation'])
+        in_datasets, self.plugin.out_datasets = self.plugin.get_datasets()
+        if len(self.stats['max']) != 0:
+            if self.pattern in ['PROJECTION', 'SINOGRAM', 'TANGENTOGRAM']:
+                Statistics.data_stats[p_num][0] = max(self.stats['max'])
+                Statistics.data_stats[p_num][1] = min(self.stats['min'])
+                Statistics.data_stats[p_num][2] = np.mean(self.stats['mean'])
+                Statistics.data_stats[p_num][3] = np.mean(self.stats['standard_deviation'])
+                Statistics.data_stats[p_num][4] = np.median(self.stats['standard_deviation'])
+            elif self.pattern in ['VOLUME_XZ', 'VOLUME_XY', 'VOLUME_YZ']:
+                Statistics.volume_stats[p_num][0] = max(self.stats['max'])
+                Statistics.volume_stats[p_num][1] = min(self.stats['min'])
+                Statistics.volume_stats[p_num][2] = np.mean(self.stats['mean'])
+                Statistics.volume_stats[p_num][3] = np.mean(self.stats['standard_deviation'])
+                Statistics.volume_stats[p_num][4] = np.median(self.stats['standard_deviation'])
+            self.plugin.out_datasets[0].meta_data.set(["stats", "max"], max(self.stats['max']))
+            self.plugin.out_datasets[0].meta_data.set(["stats", "min"], min(self.stats['min']))
+            self.plugin.out_datasets[0].meta_data.set(["stats", "mean"], np.mean(self.stats['mean']))
+            self.plugin.out_datasets[0].meta_data.set(["stats", "mean_std_dev"], np.mean(self.stats['standard_deviation']))
+            self.plugin.out_datasets[0].meta_data.set(["stats", "median_std_dev"], np.median(self.stats['standard_deviation']))
         slice_stats = np.array([self.stats['max'], self.stats['min'], self.stats['mean'],
                                 self.stats['standard_deviation']])
         self._write_stats_to_file(slice_stats, p_num)
-        #if p_num == self.plugin.exp.meta_data.plugin_list.n_plugins +1:
-        #    Statistics._post_chain()
 
     def get_data_stats(self):
         return Statistics.data_stats
@@ -95,7 +99,12 @@ class Statistics(object):
         slice_stats_dim = (slice_stats.shape[1],)
         self.hdf5 = Hdf5Utils(self.plugin.exp)
         with h5.File(filename, "a") as h5file:
-            group = h5file.create_group("/stats", track_order=None)
+            i = 1
+            group = "/stats"
+            while group in h5file:
+                group = f"/stats{i}"
+                i += 1
+            group = h5file.create_group(group, track_order=None)
             max_ds = self.hdf5.create_dataset_nofill(group, "max", slice_stats_dim, slice_stats.dtype)
             min_ds = self.hdf5.create_dataset_nofill(group, "min", slice_stats_dim, slice_stats.dtype)
             mean_ds = self.hdf5.create_dataset_nofill(group, "mean", slice_stats_dim, slice_stats.dtype)
@@ -109,20 +118,20 @@ class Statistics(object):
     def _unpad_slice(self, slice1):
         if self.plugin.pcount == 0:
             self.slice_list, self.pad = self._get_unpadded_slice_list(slice1)
-        if self.pad:
-            return slice1[self.slice_list]
-        else:
-            return slice1
+        for i in range(len(self.slice_list)):
+            if self.pad[i]:
+                slice1[i] = slice1[self.slice_list[i]]
+        return slice1
 
     def _get_unpadded_slice_list(self, slice1):
-        pad = False
         pad_width = {}
         slice_list = list(self.plugin.slice_list[0])
+        pad = [False] * len(slice_list)
         if len(slice_list) == len(slice1.shape):
             for i in range(len(self.plugin.slice_list[0])):
                 dim_width = self.plugin.slice_list[0][i].stop - self.plugin.slice_list[0][i].start
                 if dim_width != slice1.shape[i]:
-                    pad = True
+                    pad[i] = True
                     self.pad_dims.append(i)
                     pad_width[i] = (slice1.shape[i] - dim_width) // 2  # Assuming symmetrical padding
                     slice_list[i] = slice(pad_width[i], pad_width[i] + 1, 1)
