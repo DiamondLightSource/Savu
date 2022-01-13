a181141d8dcdda42918ed531f1ac34446425dace
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2022-01-13 14:40:36 +0000

    Refresh process list tests moved to a separate file

--- scripts/refresh_process_lists/savu_refresh_process_lists.py
+++ scripts/refresh_process_lists/savu_refresh_process_lists.py
@@ -13,20 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-.. module:: refresh_process_lists_test
+.. module:: savu_refresh_process_lists
    :platform: Unix
-   :synopsis: unittest test classes for plugins
+   :synopsis:
 
-.. moduleauthor:: Mark Basham <scientificsoftware@diamond.ac.uk>
+.. moduleauthor:: Jessica Verschoyle <scientificsoftware@diamond.ac.uk>
 
 """
 import os
 import argparse
-import unittest
 
 from colorama import Style
 
-import savu.test.test_process_list_utils as tplu
 import scripts.config_generator.config_utils as cu
 from scripts.config_generator.content import Content
 from scripts.config_generator.config_utils import error_catcher_savu
@@ -48,61 +46,18 @@ def __option_parser(doc=True):
     return parser if doc is True else parser.parse_args()
 
 
-class RefreshProcessListsTest(unittest.TestCase):
-    def _refresh_process_file(self, path):
-        content = Content()
-        # open
-        content.fopen(path, update=True)
-        # refresh
-        positions = content.get_positions()
-        for pos_str in positions:
-            content.refresh(pos_str)
-        # save
-        content.save(content.filename)
-        print("Content has been saved")
-
-
-def generate_test(path):
-    def test(self):
-        self._refresh_process_file(path)
-
-    return test
-
-
-def _under_revision(path):
-    files = tplu.get_test_process_list(
-        path + 'test_data/test_process_lists/under_revision')
-    return ['under_revision/' + f for f in files]
-
-
-def refresh_unittest():
-    path = os.path.dirname(os.path.realpath(__file__)).split("scripts")[0]
-
-    nxs_in_tests, plugins_in_tests = \
-        tplu.get_process_list(path + '/savu/test')
-
-    lists = tplu.get_test_process_list(path + 'test_data/process_lists') \
-        + tplu.get_test_process_list(path+'test_data/test_process_lists')
-    nxs_used = list(set(nxs_in_tests).intersection(set(lists)))
-
-    test_path = path + "/test_data/test_process_lists"
-    test_path2 = path + "/test_data/process_lists"
-
-    exclude = ["multimodal/simple_fit_test_XRF.nxs"] + _under_revision(path)
-
-    for f in [n for n in nxs_used if n not in exclude]:
-        print("Refreshing process list", f, "...")
-        if os.path.exists(os.path.join(test_path, f)):
-            path = os.path.join(test_path, f)
-        else:
-            path = os.path.join(test_path2, f)
-
-        # generates a function that calls refresh_process_file for each file
-        test = generate_test(path)
-        # then sets the function as an attribute of the test class, so that
-        # every process list is ran as an independent test
-        setattr(RefreshProcessListsTest, f"test_{path}", test)
-    unittest.main()
+def refresh_process_file(path):
+    """Refresh the process list file at path provided"""
+    content = Content()
+    # open
+    content.fopen(path, update=True)
+    # refresh
+    positions = content.get_positions()
+    for pos_str in positions:
+        content.refresh(pos_str)
+    # save
+    content.save(content.filename)
+    print("Content has been saved")
 
 
 def refresh_file(f):
@@ -112,9 +67,7 @@ def refresh_file(f):
     if os.path.isfile(f):
         if f.endswith(".nxs") or f.endswith(".savu"):
             try:
-                RefreshProcessListsTest()._refresh_process_file(
-                    os.path.abspath(f)
-                )
+                refresh_process_file(os.path.abspath(f))
             except IOError:
                 print(f"{c_on}ERROR: Problem refreshing {f} \n"
                       f"Please make sure that this is a valid Savu "
@@ -155,6 +108,3 @@ def refresh_lists():
         print("*****************   Refresh complete   ****************")
 
 
-if __name__ == "__main__":
-    cu.populate_plugins()
-    refresh_unittest()
