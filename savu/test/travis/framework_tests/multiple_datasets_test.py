45d9636a6a1f7de12e8dea6fc41b7d5896fb3d1e
Jessica Verschoyle <jessica.verschoyle@diamond.ac.uk>
2022-04-05 14:02:37 +0100

    Multiple dataset tests, pass first dataset by default

--- savu/test/travis/framework_tests/multiple_datasets_test.py
+++ savu/test/travis/framework_tests/multiple_datasets_test.py
@@ -34,7 +34,7 @@ class MultipleDatasetsTest(unittest.TestCase):
 
     def test_mm(self):
         data_file = tu.get_test_data_path('mm.nxs')
-        process_file = tu.get_test_process_path('multiple_mm_inputs_test.nxs')
+        process_file = tu.get_test_process_path('datasets/multiple_mm_inputs_test.nxs')
         run_protected_plugin_runner(tu.set_options(data_file,
                                                    process_file=process_file))
 
@@ -86,6 +86,24 @@ class MultipleDatasetsTest(unittest.TestCase):
                                                           data=all_dicts)
         exp = self.assertEqual(exp.index['in_data']['test'].get_shape(),
                                (8, 12, 15))
+
+    def test_load_once_pass_data(self):
+        """Load datasets and pass down list explicitly"""
+        data_file = tu.get_test_data_path('tomo_standard.nxs')
+        process_file = tu.get_test_process_path('datasets/ds_load_once.nxs')
+        run_protected_plugin_runner(tu.set_options(data_file,
+                                                   process_file=process_file))
+
+    def test_pass_unspecified_data(self):
+        """Load data, process data and save as a new dataset name,
+        the last plugin processes an unspecified dataset (by default the first)"""
+        data_file = tu.get_test_data_path('tomo_standard.nxs')
+        process_file = tu.get_test_process_path('datasets/ds_unspecified_in_data.nxs')
+        run_protected_plugin_runner(tu.set_options(data_file,
+                                                   process_file=process_file))
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
