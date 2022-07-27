f09f03b8a098593de0c9a17b728088a393006209
Daniil Kazantsev <dkazanc@hotmail.com>
2022-07-27 18:08:13 +0100

    inpainting module to take masks

--- savu/plugins/filters/inpainting/inpainting_tools.py
+++ savu/plugins/filters/inpainting/inpainting_tools.py
@@ -9,27 +9,52 @@ is a chunk of data missing or one needs to inpaint some data features.
         mask_range:
               visibility: basic
               dtype: list[float,float]
-              description: mask for inpainting is set as a threshold in a range.
+              description: generate mask as a threshold of the input image given the intensity range (ignored if the mask is provided).
               default: [1.0,100]
+        method:
+              visibility: intermediate
+              dtype: str
+              options: [LINEARCOMB, NONLOCAL_MARCH, DIFFUSION]
+              description: Choose inpainting method
+              default: LINEARCOMB
         iterations:
               visibility: basic
-              dtype: float
-              description: controls the smoothing level of the inpainted region.
+              dtype: int
+              description: the number of iterations to perform the inpainting (controls the smoothing level)
               default: 50
         windowsize_half:
-              visibility: basic
+              visibility: intermediate
               dtype: int
-              description: half-size of the smoothing window.
+              description: half-size of the smoothing window, increase size for more smoothing
               default: 3
+              dependency:
+                method: [LINEARCOMB]
+        search_window_increment:
+              visibility: advanced
+              dtype: int
+              description: the increment value with which the searching window is growing in iterations
+              default: 1
+              dependency:
+                method: [NONLOCAL_MARCH]
         sigma:
               visibility: basic
               dtype: float
-              description: maximum value for the inpainted region.
+              description: maximum intensity value for the inpainted region.
               default: 0.5
-        pattern:
+              dependency:
+                method: [LINEARCOMB]
+        regularisation_parameter:
               visibility: basic
-              dtype: str
-              description: Pattern to apply these to.
-              default: SINOGRAM
-
+              dtype: float
+              description: regularisation parameter for diffusion
+              default: 1000
+              dependency:
+                method: [DIFFUSION]
+        time_marching_parameter:
+              visibility: intermediate
+              dtype: float
+              description: ensuring convergence of the diffusion scheme
+              default: 0.00001
+              dependency:
+                method: [DIFFUSION]
         """
