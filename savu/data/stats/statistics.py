5b41020fdddead2a2e098f950a6c3393971258cf
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2022-08-11 10:08:47 +0100

    Small change to pre-run plus 4d bugfix.

--- savu/data/stats/statistics.py
+++ savu/data/stats/statistics.py
@@ -65,7 +65,7 @@ class Statistics(object):
                 self._set_pattern_info()
         if self.calc_stats:
             Statistics._any_stats = True
-        self._setup_4d()
+            self._setup_4d()
         self._setup_iterative()
 
     def _setup_iterative(self):
@@ -79,7 +79,7 @@ class Statistics(object):
     def _setup_4d(self):
         try:
             in_dataset, out_dataset = self.plugin.get_datasets()
-            if in_dataset[0].data_info["nDims"] == 4 and len(out_dataset) != 0:
+            if in_dataset[0].data_info["nDims"] == 4:
                 self._4d = True
                 shape = out_dataset[0].data_info["shape"]
                 self._volume_total_points = 1
